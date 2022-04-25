d3046bf0d3117a0d4ba6af5e9c38276f874148f1
nicholas <znicholasbrown@gmail.com>
2022-04-25 19:22:46 -0400

    Scaffolding

---
+++ README.md
@@ -0,0 +1,10 @@
+# Prefect Design
+A collection of low-level Vue components. 
+
+## Development
+```
+npm run serve
+```
+
+## Installation
+_Coming soon!_
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  },
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Miter Design',
+      fileName: (format) => `miter-design.${format}.js`
+    },
+    rollupOptions: {
+      // ensures vue isn't added to the bundle
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        // Provide vue as a global variable to use in the UMD build
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
