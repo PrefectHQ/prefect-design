bd68dfb3c618d2cef75d0b4e2b31edad79a6437b
nicholas <znicholasbrown@gmail.com>
2022-04-25 21:08:45 -0400

    Commiting what i have for now - not working

--- demo/App.vue
+++ demo/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div v-cloak class="pt-5 px-10 pb-10">
-    <div>
-      <router-link class="mr-4" to="/">
-        Components
-      </router-link>
+  <div class="min-h-full">
+    <div class="py-10">
+      <header>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold leading-tight text-gray-900">
+            Dashboard
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <!-- Replace with your content -->
+          <div class="px-4 py-8 sm:px-0">
+            <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+          </div>
+          <!-- /End replace -->
+        </div>
+      </main>
     </div>
-
-    <router-view />
   </div>
 </template>
 
 <script lang="ts" setup>
-
+  // Hello!
 </script>
-
-<style>
-[v-cloak] {
-  display: none;
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-
-
-module.exports = {
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 }
