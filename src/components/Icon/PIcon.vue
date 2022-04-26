a855a10da3ad055d911a46996ed49ef4baf9007f
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-26 17:25:27 -0500

    created PIcon component and types

---
+++ src/components/Icon/PIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <component :is="heroIcons[icon]" />
+</template>
+
+
+<script lang="ts" setup>
+  import * as heroIcons from '@heroicons/vue/solid'
+  import { Icon } from '@/types/icon'
+
+  defineProps<{ icon: Icon }>()
+</script>
