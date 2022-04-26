8f09f0c75b537ea87f20cddc45db677181e75ee1
nicholas <znicholasbrown@gmail.com>
2022-04-26 13:41:48 -0400

    Update config with inter font and prefect color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './index.html',
@@ -5,7 +7,26 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        prefect: {
+          50: '#F2F6FF',
+          100: '#E6EDFF',
+          200: '#C0D3FF',
+          300: '#9AB8FE',
+          400: '#4E82FE',
+          500: '#024DFD',
+          600: '#0245E4',
+          700: '#023ABE',
+          800: '#012E98',
+          900: '#01267C',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
