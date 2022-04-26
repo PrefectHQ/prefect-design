ab70a60447a30425159ee23234d9e94d24be83cf
Natalie Smith <thatgalnatalie@gmail.com>
2022-04-26 12:01:53 -0700

    inital commit

--- demo/App.vue
+++ demo/App.vue
@@ -11,7 +11,12 @@
       <main>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <!-- Replace with your content -->
-          <p-tag />
+          <p-tag>
+            <template #icon>
+              <BeakerIcon class="h-5 w-5 text-blue-500 mr-px" />
+            </template>
+            tag
+          </p-tag>
           <!-- /End replace -->
         </div>
       </main>
@@ -20,6 +25,8 @@
 </template>
 
 <script lang="ts" setup>
+  import { BeakerIcon } from '@heroicons/vue/solid'
   import PTag from '../src/components/Tag/PTag.vue'
+
   // Hello!
 </script>
--- src/components/Tag/PTag.vue
+++ src/components/Tag/PTag.vue
@@ -1,7 +1,21 @@
 <template>
-  <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800"> Badge </span>
+  <span
+    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800"
+    :class="classes"
+  >
+    <slot name="icon" />
+    <slot />
+  </span>
 </template>
 
 <script lang="ts" setup>
-  // This is where a script should go
+  import { computed, withDefaults } from 'vue'
+
+  const props = withDefaults(defineProps<{
+    disabled?: boolean,
+  }>(), {
+  })
+  const classes = computed(() => ({
+    'cursor-not-allowed': props.disabled,
+  }))
 </script>
