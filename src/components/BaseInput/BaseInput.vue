b68b81aada0e3b2c4319d6ac7bc48148eda5adbf
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-27 13:37:00 -0500

    refactored out duplicate code into base-input component

---
+++ src/components/BaseInput/BaseInput.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="base-input" :class="classes" @click="control?.focus()">
+    <div v-if="prepend" class="base-input__prepend">
+      {{ prepend }}
+    </div>
+    <slot name="prepend" />
+    <slot ref="control" name="control" :attrs="$attrs" />
+    <div v-if="append" class="base-input__append">
+      {{ append }}
+    </div>
+    <slot name="append" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed, ref } from 'vue'
+
+  export default defineComponent({
+    name: 'BaseInput',
+    expose: [],
+    inheritAttrs: false,
+  })
+</script>
+
+<script lang="ts" setup>
+  import { State } from '@/types/state'
+
+  const props = defineProps<{
+    state?: State,
+    prepend?: string,
+    append?: string,
+  }>()
+
+  const control = ref<HTMLInputElement>()
+
+  const classes = computed(() => ({
+    'base-input--valid': props.state?.valid,
+    'base-input--invalid': props.state?.invalid,
+    'base-input--changed': props.state?.changed,
+    'base-input--touched': props.state?.touched,
+    'base-input--untouched': props.state?.untouched,
+    'base-input--pristine': props.state?.pristine,
+    'base-input--dirty': props.state?.dirty,
+    'base-input--pending': props.state?.pending,
+    'base-input--required': props.state?.required,
+    'base-input--validated': props.state?.validated,
+    'base-input--passed': props.state?.passed,
+    'base-input--failed': props.state?.failed,
+  }))
+</script>
+
+<style>
+.base-input { @apply
+  w-full
+  border
+  flex
+  items-center
+  focus-within:ring-1
+  focus-within:border-prefect-600
+  focus-within:ring-prefect-600
+  border-gray-300
+  shadow-sm
+  rounded-md
+}
+
+.base-input--disabled { @apply
+  cursor-not-allowed
+  opacity-50
+}
+
+.base-input__prepend,
+.base-input__append { @apply
+  px-2
+  flex
+  flex-col
+  whitespace-nowrap
+  justify-center
+  self-stretch
+  text-sm
+  border-gray-300
+}
+
+.base-input__prepend { @apply
+  border-r
+}
+
+.base-input__append { @apply
+  border-l
+}
+
+.base-input--failed { @apply
+  border-red-600
+  focus-within:border-red-600
+  focus-within:ring-red-600
+}
+</style>
--- src/components/NumberInput/PNumberInput.vue
+++ src/components/NumberInput/PNumberInput.vue
@@ -1,49 +1,23 @@
 <template>
-  <div class="p-number-input" :class="classes" @click="control?.focus()">
-    <div v-if="prepend" class="p-number-input__prepend">
-      {{ prepend }}
-    </div>
-    <slot name="prepend" />
-    <input
-      ref="control"
-      v-model.number="value"
-      type="number"
-      class="p-number-input__control"
-      v-bind="$attrs"
-      @keypress="preventNonNumericalInput($event)"
-    >
-    <div v-if="append" class="p-number-input__append">
-      {{ append }}
-    </div>
-    <slot name="append" />
-  </div>
+  <BaseInput class="p-text-input">
+    <slot v-for="(_, name) in $slots" :name="name" />
+    <template #control>
+      <input v-model.number="value" type="number" class="p-text-input__control" @keypress="preventNonNumericalInput">
+    </template>
+  </BaseInput>
 </template>
 
-<script lang="ts">
-  import { defineComponent, computed, ref } from 'vue'
-
-  export default defineComponent({
-    name: 'PNumberInput',
-    expose: [],
-    inheritAttrs: false,
-  })
-</script>
-
 <script lang="ts" setup>
-  import { State } from '@/types/state'
+  import { computed } from 'vue'
+  import BaseInput from '@/components/BaseInput'
 
   const props = defineProps<{
     modelValue: number | null,
-    state?: State,
-    prepend?: string,
-    append?: string,
   }>()
 
   const emits = defineEmits<{
     (event: 'update:modelValue', value: number | null): void,
   }>()
-
-  const control = ref<HTMLInputElement>()
 
   const value = computed({
     get() {
@@ -65,71 +39,12 @@
       event.preventDefault()
     }
   }
-
-  const classes = computed(() => ({
-    'p-number-input--valid': props.state?.valid,
-    'p-number-input--invalid': props.state?.invalid,
-    'p-number-input--changed': props.state?.changed,
-    'p-number-input--touched': props.state?.touched,
-    'p-number-input--untouched': props.state?.untouched,
-    'p-number-input--pristine': props.state?.pristine,
-    'p-number-input--dirty': props.state?.dirty,
-    'p-number-input--pending': props.state?.pending,
-    'p-number-input--required': props.state?.required,
-    'p-number-input--validated': props.state?.validated,
-    'p-number-input--passed': props.state?.passed,
-    'p-number-input--failed': props.state?.failed,
-  }))
 </script>
 
 <style>
-.p-number-input { @apply
-  w-full
-  border
-  flex
-  items-center
-  focus-within:ring-1
-  focus-within:border-prefect-600
-  focus-within:ring-prefect-600
-  border-gray-300
-  shadow-sm
-  rounded-md
-}
-
-.p-number-input--disabled { @apply
-  cursor-not-allowed
-  opacity-50
-}
-
 .p-number-input__control { @apply
   block
   w-full
   rounded-md
-}
-
-.p-number-input__prepend,
-.p-number-input__append { @apply
-  px-2
-  flex
-  flex-col
-  whitespace-nowrap
-  justify-center
-  self-stretch
-  text-sm
-  border-gray-300
-}
-
-.p-number-input__prepend { @apply
-  border-r
-}
-
-.p-number-input__append { @apply
-  border-l
-}
-
-.p-number-input--failed { @apply
-  border-red-600
-  focus-within:border-red-600
-  focus-within:ring-red-600
 }
 </style>
