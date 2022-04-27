adb9dba2f37fba3240e05617f58ce21b19172fa2
Natalie Smith <thatgalnatalie@gmail.com>
2022-04-27 13:02:40 -0700

    update demo

--- demo/Tag.vue
+++ demo/Tag.vue
@@ -1,33 +1,76 @@
 <template>
-  <p-tag icon="TagIcon">
-    testing
-  </p-tag>
-
-  <p-tag v-for="index in ['one', 'two', 'three']" :key="index">
+  <p-tag v-for="index in 3" :key="index">
     {{ index }}
   </p-tag>
-
-  <div>
-    <p-tag v-for="index in ['one', 'two', 'three']" :key="index" icon="TagIcon">
-      {{ index }}
+  <div class="my-2">
+    <p-tag v-for="item in classes" :key="item.name" :class="item.className" class="tag" icon="TagIcon">
+      {{ item.name }}
     </p-tag>
   </div>
 
-  <div>
-    <p-tag v-for="state in states" :key="state" :color="state" icon="TagIcon">
-      {{ state }}
-    </p-tag>
-  </div>
-
-  <div>
-    <p-tag v-for="state in states" :key="state" :color="state" flat icon="TagIcon">
-      {{ state }}
+  <div class="my-2">
+    <p-tag v-for="item in ['completed', 'cancelled', 'failed']" :key="item" icon="TagIcon" flat :class="`tag--${item}-flat`">
+      {{ item }}
     </p-tag>
   </div>
 </template>
 
 <script lang="ts" setup>
   import PTag from '../src/components/Tag/PTag.vue'
-  const states = ['completed', 'cancelled', 'failed', 'pending', 'running', 'scheduled']
+  const classes = [
+    { className: 'tag--completed', name: 'Completed'  },
+    { className: 'tag--cancelled', name: 'Cancelled'  },
+    { className:  'tag--failed',  name: 'Failed'  },
+    { className: 'tag--pending',  name: 'Pending'  },
+    { className:  'tag--running',  name: 'Running'  },
+    { className:  'tag--scheduled', name: 'Scheduled'  },
+  ]
 </script>
+
+<style>
+.tag {@apply
+ rounded-md
+}
+.tag:is(.tag--completed,.tag--cancelled, .tag--failed, .tag--running) {@apply
+  text-slate-50
+}
+
+.tag:is(.tag--pending,.tag--scheduled) {@apply
+  text-slate-800
+}
+
+.tag--completed {@apply
+  bg-state-colors-completed
+}
+
+.tag--completed-flat .p-tag--icon{@apply
+  text-state-colors-completed
+}
+.tag--cancelled-flat .p-tag--icon{@apply
+  text-state-colors-cancelled
+}
+.tag--failed-flat .p-tag--icon{@apply
+  text-state-colors-failed
+}
+
+.tag--cancelled { @apply
+  bg-state-colors-cancelled
+}
+
+.tag--failed{ @apply
+  bg-state-colors-failed
+}
+
+.tag--pending{ @apply
+  bg-state-colors-pending
+}
+
+.tag--running{ @apply
+  bg-state-colors-running
+}
+
+.tag--scheduled { @apply
+  bg-state-colors-scheduled
+}
+</style>
 
--- src/components/Tag/PTag.vue
+++ src/components/Tag/PTag.vue
@@ -16,14 +16,13 @@
   import { Icon } from '@/types/icon'
 
   const props = defineProps<{
-    color?: 'completed' | 'cancelled' | 'failed' | 'pending' | 'running' | 'scheduled',
     flat?: boolean,
     icon?: Icon,
   }>()
 
-  const states = ['completed', 'cancelled', 'failed', 'pending', 'running', 'scheduled']
-  const mappedStates = computed(() => states.map(state => ({ [`p-tag--${state}`]: props.color?.toLowerCase() === state })))
-  const classes  = computed(() => [{ 'p-tag--flat': props.flat }, ...mappedStates.value])
+  const classes = computed(() => ({
+    'p-tag--flat': props.flat,
+  }))
 </script>
 
 <style>
@@ -38,78 +37,10 @@
   py-0.5
   text-sm
 }
-.p-tag:is(
-  .p-tag--completed,
-  .p-tag--cancelled,
-  .p-tag--failed,
-  .p-tag--pending,
-  .p-tag--running,
-  .p-tag--scheduled) {@apply
-    rounded-md
-}
-
-.p-tag--flat:is(
-  .p-tag--completed,
-  .p-tag, .p-tag--cancelled,
-  .p-tag--failed,
-  .p-tag--pending,
-  .p-tag--running,
-  .p-tag--scheduled){ @apply
-    bg-transparent
-}
-
-.p-tag--flat:is(
-  .p-tag--completed,
-  .p-tag, .p-tag--cancelled,
-  .p-tag--failed,
-  .p-tag--pending,
-  .p-tag--running,
-  .p-tag--scheduled) .p-tag--label{ @apply
+.p-tag--flat {@apply
+  bg-transparent
   text-slate-700
   font-semibold
-}
-
-.p-tag--completed { @apply
-  text-slate-50
-  bg-state-colors-completed
-}
-.p-tag--completed:is(.p-tag--flat) .p-tag--icon { @apply
-  text-state-colors-completed
-}
-.p-tag--cancelled { @apply
-  text-slate-50
-  bg-state-colors-cancelled
-}
-.p-tag--cancelled:is(.p-tag--flat) .p-tag--icon { @apply
-  text-state-colors-cancelled
-}
-.p-tag--failed{ @apply
-  text-slate-50
-  bg-state-colors-failed
-}
-.p-tag--failed:is(.p-tag--flat) .p-tag--icon { @apply
-  text-state-colors-failed
-}
-.p-tag--pending{ @apply
-  text-slate-800
-  bg-state-colors-pending
-}
-.p-tag--pending:is(.p-tag--flat) .p-tag--icon { @apply
-  text-state-colors-pending
-}
-.p-tag--running{ @apply
-  text-slate-50
-  bg-state-colors-running
-}
-.p-tag--running:is(.p-tag--flat) .p-tag--icon { @apply
-  text-state-colors-running
-}
-.p-tag--scheduled { @apply
-  text-slate-800
-  bg-state-colors-scheduled
-}
-.p-tag--scheduled:is(.p-tag--flat) .p-tag--icon { @apply
-  text-state-colors-scheduled
 }
 .p-tag--icon {@apply
   w-4
