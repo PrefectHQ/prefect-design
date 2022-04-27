696a9dc7996d72acf74e8d9c042a50b451814555
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-27 14:24:06 -0500

    added p-textarea component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,18 +1,21 @@
 import { default as PButton } from './Button'
 import { default as PIcon } from './Icon'
 import { default as PNumberInput } from './NumberInput'
+import { default as PTextareaInput } from './TextareaInput'
 import { default as PTextInput } from './TextInput'
 
 export {
   PButton,
   PIcon,
-  PTextInput,
-  PNumberInput
+  PNumberInput,
+  PTextareaInput,
+  PTextInput
 }
 
 export const installs = [
   PButton.install,
   PIcon.install,
-  PTextInput.install,
   PNumberInput.install,
+  PTextareaInput.install,
+  PTextInput.install,
 ]
