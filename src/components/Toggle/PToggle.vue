a6b9feb67376704864c2f555e484681120b1ace2
Natalie Smith <thatgalnatalie@gmail.com>
2022-04-28 10:04:37 -0700

    add classes

--- src/components/Toggle/PToggle.vue
+++ src/components/Toggle/PToggle.vue
@@ -1,10 +1,11 @@
 <template>
-  <SwitchGroup as="div" class="flex items-center">
+  <SwitchGroup as="div" class="p-toggle">
     <SwitchLabel v-if="$slots.prepend" as="span" class="mr-3">
       <slot name="prepend" />
     </SwitchLabel>
-    <Switch v-model="toggleModel" class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-prefect-500 bg-gray-200" :class="toggleClasses" :disabled="disabled">
-      <span aria-hidden="true" class="pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200" :class="toggleTranslate" />
+
+    <Switch v-model="value" class="p-toggle__switch" :class="classes.toggle" :disabled="disabled">
+      <span aria-hidden="true" class="p-toggle__switch-aria" :class="classes.translate" />
     </Switch>
     <SwitchLabel v-if="$slots.append" as="span" class="ml-3">
       <slot name="append" />
@@ -25,7 +26,7 @@
     (event: 'update:modelValue', value: boolean): void,
   }>()
 
-  const toggleModel = computed({
+  const value = computed({
     get() {
       return props.modelValue
     },
@@ -33,13 +34,62 @@
       emits('update:modelValue', value)
     },
   })
-  const toggleTranslate = computed(() => [toggleModel.value ? 'translate-x-5' : 'translate-x-0'])
-  const toggleClasses = computed(() => ({ 'p-toggle--disable': props.disabled, 'bg-prefect-500': toggleModel.value }))
+
+
+  const classes = computed(() => ({
+    translate: {
+      'translate-x-5': props.modelValue,
+      'translate-x-0': !props.modelValue,
+    },
+    toggle: {
+      'p-toggle__switch-disabled': props.disabled,
+      'p-toggle__switch-enabled': props.modelValue,
+    },
+  }))
 </script>
 
 <style>
-.p-toggle--disable {@apply
+.p-toggle { @apply
+  flex
+  items-center
+}
+.p-toggle__switch { @apply
+  relative
+  inline-flex
+  flex-shrink-0
+  h-6 w-11
+  border-2
+  border-transparent
+  rounded-full
+  cursor-pointer
+  transition-colors
+  ease-in-out
+  duration-200
+  focus:outline-none
+  focus:ring-2
+  focus:ring-offset-2
+  focus:ring-prefect-500
+  bg-gray-200
+}
+.p-toggle__switch-aria { @apply
+  pointer-events-none
+  inline-block
+  h-5
+  w-5
+  rounded-full
+  bg-white
+  shadow
+  transform
+  ring-0
+  transition
+  ease-in-out
+  duration-200
+}
+.p-toggle__switch-disabled { @apply
   cursor-not-allowed
   opacity-50
+}
+.p-toggle__switch-enabled { @apply
+  bg-prefect-500
 }
 </style>
