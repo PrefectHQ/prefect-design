0651930ccb16999dcc3c105b4bfaa63144637da9
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-28 10:57:18 -0500

    missed some 'inputs'

--- src/components/Checkbox/PCheckbox.vue
+++ src/components/Checkbox/PCheckbox.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="p-checkbox-input" :class="classes">
-    <label class="p-checkbox-input__label">
+  <div class="p-checkbox" :class="classes">
+    <label class="p-checkbox__label">
       <slot name="label">
-        <div class="p-checkbox-input__label-text">
+        <div class="p-checkbox__label-text">
           {{ label }}
         </div>
       </slot>
       <input
         v-model="value"
         type="checkbox"
-        class="p-checkbox-input__control"
+        class="p-checkbox__control"
         :disabled="disabled"
         v-bind="$attrs"
       >
@@ -54,34 +54,34 @@
   })
 
   const classes = computed(() => ({
-    'p-checkbox-input--disabled': props.disabled,
-    'p-checkbox-input--valid': !!props.state?.valid,
-    'p-checkbox-input--invalid': !!props.state?.invalid,
-    'p-checkbox-input--changed': !!props.state?.changed,
-    'p-checkbox-input--touched': !!props.state?.touched,
-    'p-checkbox-input--untouched': !!props.state?.untouched,
-    'p-checkbox-input--pristine': !!props.state?.pristine,
-    'p-checkbox-input--dirty': !!props.state?.dirty,
-    'p-checkbox-input--pending': !!props.state?.pending,
-    'p-checkbox-input--required': !!props.state?.required,
-    'p-checkbox-input--validated': !!props.state?.validated,
-    'p-checkbox-input--passed': !!props.state?.passed,
-    'p-checkbox-input--failed': !!props.state?.failed,
+    'p-checkbox--disabled': props.disabled,
+    'p-checkbox--valid': !!props.state?.valid,
+    'p-checkbox--invalid': !!props.state?.invalid,
+    'p-checkbox--changed': !!props.state?.changed,
+    'p-checkbox--touched': !!props.state?.touched,
+    'p-checkbox--untouched': !!props.state?.untouched,
+    'p-checkbox--pristine': !!props.state?.pristine,
+    'p-checkbox--dirty': !!props.state?.dirty,
+    'p-checkbox--pending': !!props.state?.pending,
+    'p-checkbox--required': !!props.state?.required,
+    'p-checkbox--validated': !!props.state?.validated,
+    'p-checkbox--passed': !!props.state?.passed,
+    'p-checkbox--failed': !!props.state?.failed,
   }))
 </script>
 
 <style>
-.p-checkbox-input--disabled { @apply
+.p-checkbox--disabled { @apply
   cursor-not-allowed
   opacity-50
 }
 
-.p-checkbox-input--failed { @apply
+.p-checkbox--failed { @apply
   border-red-600
   focus:ring-red-600
 }
 
-.p-checkbox-input__control { @apply
+.p-checkbox__control { @apply
   focus:ring-prefect-600
   h-4
   w-4
@@ -90,7 +90,7 @@
   rounded
 }
 
-.p-checkbox-input__label { @apply
+.p-checkbox__label { @apply
   text-sm
   font-medium
   text-gray-700
@@ -99,7 +99,7 @@
   items-center
 }
 
-.p-checkbox-input__label .p-checkbox-input__label-text { @apply
+.p-checkbox__label .p-checkbox__label-text { @apply
   order-last
 }
 </style>
