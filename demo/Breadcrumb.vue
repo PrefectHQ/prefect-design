1d10b38596210e1c473b327e9d8f3e26844d361e
Joe Clay <joeclayallday@gmail.com>
2022-05-03 13:21:43 -0500

    Add active color states

--- demo/Breadcrumb.vue
+++ demo/Breadcrumb.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="mb-4 border-4 border-dashed border-gray-200 rounded-lg p-4">
-    <p class="text-2xl mb-1"><a href="#" class="text-blue-500 hover:border-b-2 hover:border-blue-500">ETL</a><span class="p-1 text-gray-400">/</span>hungry-hippo</p>
+    <p class="text-2xl mb-1"><a href="#" class="text-blue-500 hover:border-b-2 hover:border-blue-500 active:text-blue-700 active:border-blue-700">ETL</a><span class="p-1 text-gray-400">/</span>hungry-hippo</p>
   </div>
   <div class="mb-4 border-4 border-dashed border-gray-200 rounded-lg p-4">
-    <p class="text-2xl mb-1"><a href="#" class="text-blue-500 hover:border-b-2 hover:border-blue-500">ETL</a><span class="p-1 text-gray-400">/</span>hungry-hippo</p>
-    <p class="text-xs text-gray-500"><a href="#" title="Copy"><p-icon class="w-4 h-4 inline hover:text-blue-500" icon="DocumentDuplicateIcon" /></a> Flow ID: 987a6d29-4a6e-48ab-aec6-b2905a7a6b75</p>
+    <p class="text-2xl mb-1"><a href="#" class="text-blue-500 hover:border-b-2 hover:border-blue-500 active:text-blue-700 active:border-blue-700">ETL</a><span class="p-1 text-gray-400">/</span>hungry-hippo</p>
+    <p class="text-xs text-gray-500"><a href="#" title="Copy"><p-icon class="w-4 h-4 inline hover:text-blue-500 active:text-blue-700" icon="DocumentDuplicateIcon" /></a> Flow ID: 987a6d29-4a6e-48ab-aec6-b2905a7a6b75</p>
   </div>
   <div class="mb-4 border-4 border-dashed border-gray-200 rounded-lg p-4">
     <div class="flex gap-2">
       <div class="flex-1">
-        <p class="text-2xl mb-1"><a href="#" class="text-blue-500 hover:border-b-2 hover:border-blue-500">ETL</a><span class="p-1 text-gray-400">/</span>hungry-hippo</p>
-        <p class="text-xs text-gray-500"><a href="#" title="Copy"><p-icon class="w-4 h-4 inline hover:text-blue-500" icon="DocumentDuplicateIcon" /></a> Flow ID: 987a6d29-4a6e-48ab-aec6-b2905a7a6b75</p>
+        <p class="text-2xl mb-1"><a href="#" class="text-blue-500 hover:border-b-2 hover:border-blue-500 active:text-blue-700 active:border-blue-700">ETL</a><span class="p-1 text-gray-400">/</span>hungry-hippo</p>
+        <p class="text-xs text-gray-500"><a href="#" title="Copy"><p-icon class="w-4 h-4 inline hover:text-blue-500 active:text-blue-700" icon="DocumentDuplicateIcon" /></a> Flow ID: 987a6d29-4a6e-48ab-aec6-b2905a7a6b75</p>
       </div>
       <div class="order-last">
         <div class="flex gap-2 mb-4">
