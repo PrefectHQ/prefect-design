c165bab0c1f909333af6aedbf185c10a6da3afb3
Natalie Smith <thatgalnatalie@gmail.com>
2022-05-04 15:15:03 -0700

    Update PTagWrapper.vue

--- src/components/TagWrapper/PTagWrapper.vue
+++ src/components/TagWrapper/PTagWrapper.vue
@@ -10,7 +10,7 @@
       </template>
     </slot>
 
-    <p-tag v-show="hasOverflowChildren" class="p-tag-wrapper__tag-overflow">
+    <p-tag v-show="hasOverflowChildren" ref="overflowTag" class="p-tag-wrapper__tag-overflow">
       +{{ overflowChildren }}
     </p-tag>
   </div>
@@ -23,17 +23,13 @@
   }>()
   const container: Ref<HTMLDivElement | undefined> = ref()
   const overflowChildren = ref(0)
+  const overflowTag = ref()
 
   let resizeObserver: ResizeObserver | null = null
   const hasOverflowChildren = computed(() => overflowChildren.value > 0)
   function setChildIsVisible(child: HTMLElement): boolean {
     const containerWidth = container.value?.parentElement?.offsetWidth ?? 0
-    /*
-    note: overflowTagWidth is a fixed width for the tag that displays the overflow number e.g. (+13)
-    This should be replaced with a calculated width instead
-    The fixed width is used to account for the content in the overflow tag
-    */
-    const overflowTagWidth = 55
+    const overflowTagWidth = overflowTag.value.$el?.clientWidth ?? 55
     if (child.classList.contains('p-tag-wrapper__tag--hidden')) {
       child.classList.add('p-tag-wrapper__tag--invisible')
       child.classList.remove('p-tag-wrapper__tag--hidden')
@@ -46,8 +42,9 @@
       child.classList.add('p-tag-wrapper__tag--hidden')
     } else {
       child.classList.remove('p-tag-wrapper__tag--hidden')
-      child.classList.remove('p-tag-wrapper__tag--invisible')
     }
+    child.classList.remove('p-tag-wrapper__tag--invisible')
+
     return childOverflowsContainer
   }
 
