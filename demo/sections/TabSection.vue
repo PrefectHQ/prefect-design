2a68b16793d884115e4169d381522118264a65d1
Natalie Smith <thatgalnatalie@gmail.com>
2022-05-10 06:15:30 -0700

    Create TabSection.vue

---
+++ demo/sections/TabSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <Section heading="Tabs">
+    <p-tabs v-model="selectedTab" :tabs="tabs">
+      <template #tab-heading-0="{ tab }">
+        <p-icon icon="Prefect" class="w-4 h-4 mr-2" />
+        <span>{{ tab }}</span>
+      </template>
+    </p-tabs>
+
+
+    Selected tab index: {{ selectedTab }}
+  </Section>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import Section from '../components/Section.vue'
+  const tabs = ['Tab One', 'Tab Two', 'Tab Three']
+  const selectedTab = ref()
+</script>
