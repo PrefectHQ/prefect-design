6ceba78610b578352f744028d51eea2a61794b24
Evan Sutherland <stackoverfloweth@gmail.com>
2022-05-10 20:13:07 -0500

    month-picker autofocus on selected

--- src/components/DatePicker/PMonthPicker.vue
+++ src/components/DatePicker/PMonthPicker.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="p-month-picker">
     <template v-for="(month, index) in monthNames" :key="month">
-      <p-button class="p-month-picker__month" size="xs" :flat="!isSelected(index)" @click="setMonth(index)">
-        {{ month }}
+      <p-button
+        class="p-month-picker__month"
+        size="xs"
+        :flat="!isSelected(index)"
+        @click="selectedMonth = index"
+      >
+        <span ref="monthElements" :data-month="index">
+          {{ month }}
+        </span>
       </p-button>
     </template>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { computed } from 'vue'
+  import { computed, nextTick, onMounted, ref } from 'vue'
   import PButton from '@/components/Button'
   import { getStartOfDay, monthNames } from '@/types/date'
 
@@ -21,6 +28,8 @@
     (event: 'update:modelValue', value: Date | null): void,
   }>()
 
+  const monthElements = ref<HTMLElement[]>([])
+
   const selectedDate = computed({
     get() {
       return props.modelValue ?? new Date(getStartOfDay())
@@ -30,17 +39,30 @@
     },
   })
 
+  const selectedMonth = computed({
+    get() {
+      return selectedDate.value.getUTCMonth()
+    },
+    set(month: number) {
+      const value = new Date(selectedDate.value)
+
+      value.setUTCMonth(month)
+
+      selectedDate.value = value
+    },
+  })
+
   function isSelected(month: number): boolean {
-    return selectedDate.value.getUTCMonth() === month
+    return selectedMonth.value === month
   }
 
-  function setMonth(month: number): void {
-    const value = new Date(selectedDate.value)
+  onMounted(() => {
+    const element = monthElements.value.find(node => node.dataset.month === selectedMonth.value.toString())
 
-    value.setUTCMonth(month)
-
-    selectedDate.value = value
-  }
+    if (element) {
+      nextTick(() => element.parentElement?.focus())
+    }
+  })
 </script>
 
 <style>
