95e3d2d3b6351ef3609256e6b30172618091554b
nicholas <znicholasbrown@gmail.com>
2022-05-11 19:52:59 -0400

    Add vue router

--- demo/App.vue
+++ demo/App.vue
@@ -1,58 +1,18 @@
 <template>
   <div class="min-h-full">
-    <div class="py-10">
-      <header>
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold leading-tight text-gray-900">
-            Dashboard
-          </h1>
-        </div>
-      </header>
-      <main>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <PopOverSection />
-
-          <TypographySection />
-
-          <BreadCrumbsSection />
-
-          <ButtonsSection />
-
-          <FormsSection />
-
-          <TogglesSection />
-
-          <TagsSection />
-
-          <ToastsSection />
-
-          <ModalsSection />
-
-          <IconsSection />
-
-          <CodeSection />
-
-          <TablesSection />
-
-          <IconTextSection />
-        </div>
-      </main>
-    </div>
+    <!-- TODO: Replace this nav with sidebar nav from the library -->
+    <!-- TODO: Break out icons, typography, colors (new), compositions (new) into their own pages -->
+    <nav class="max-w-7xl mx-auto px-4 pt-10 sm:px-6 lg:px-8">
+      <router-link to="/components">
+        Components
+      </router-link>
+    </nav>
+    <router-view class="max-w-7xl mx-auto sm:px-6 lg:px-8" />
   </div>
 </template>
 
-<script lang="ts" setup>
-  import BreadCrumbsSection from './sections/BreadCrumbsSection.vue'
-  import ButtonsSection from './sections/ButtonsSection.vue'
-  import CodeSection from './sections/CodeSection.vue'
-  import FormsSection from './sections/FormsSection.vue'
-  import IconsSection from './sections/IconsSection.vue'
-  import IconTextSection from './sections/IconTextSection.vue'
-  import ModalsSection from './sections/ModalsSection.vue'
-  import PopOverSection from './sections/PopOverSection.vue'
-  import TablesSection from './sections/TablesSection.vue'
-  import TagsSection from './sections/TagsSection.vue'
-  import ToastsSection from './sections/ToastsSection.vue'
-  import TogglesSection from './sections/TogglesSection.vue'
-  import TypographySection from './sections/TypographySection.vue'
-</script>
+<style>
+a {
+  text-decoration: underline;
+}
+</style>
