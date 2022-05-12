1b66037136934e6a17cc9fd5b6fd35101a0e1f02
Marichka Offen <marichka@prefect.io>
2022-05-12 14:30:16 -0400

    Develop Context nav item

--- src/components/ContextNavItem/PContextNavItem.vue
+++ src/components/ContextNavItem/PContextNavItem.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    Context nav item
-  </div>
+  <a
+    :href="href"
+    class="p-context-nav-item"
+    :class="{ 'p-context-nav-item--active': isActive }"
+  >
+    <p-icon :icon="icon" class="p-context-nav-item__icon" />
+    <span>{{ title }}</span>
+  </a>
 </template>
+
+<script setup lang="ts">
+  defineProps<{
+    href: string,
+    title?: string,
+    icon?: string,
+    isActive: boolean,
+  }>()
+</script>
+
+<style>
+.p-context-nav-item {
+  @apply
+  flex
+  items-center
+  gap-3
+  text-sm
+  leading-5
+  font-medium
+  py-2.5
+  rounded-md
+  -mx-2
+  px-2
+  transition
+  duration-200
+  ease-linear
+  hover:bg-slate-600
+  focus:bg-white/25
+}
+
+.p-context-nav-item__icon{
+  @apply
+  h-6
+  w-6
+}
+
+.p-context-nav-item--active {
+  @apply
+  bg-white/25
+}
+</style>
