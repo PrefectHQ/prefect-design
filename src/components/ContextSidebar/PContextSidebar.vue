dd184a069ff8c5a8f074f63904dea1bd3267c070
Marichka Offen <marichka@prefect.io>
2022-05-12 16:19:28 -0400

    Context Sidebar layout fix

--- src/components/ContextSidebar/PContextSidebar.vue
+++ src/components/ContextSidebar/PContextSidebar.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="p-context-sidebar">
-    <div class="p-context-sidebar__nav-items">
+    <div>
       <slot name="header" />
-      <slot name="upper-links" />
+      <div class="p-context-sidebar__nav-items">
+        <slot name="upper-links" />
+      </div>
     </div>
     <div class="p-context-sidebar__nav-items">
       <slot name="bottom-links" />
@@ -15,14 +17,12 @@
   @apply
   bg-slate-700
   text-slate-300
-  font-sans
   fixed
   w-screen
-  flex
-  flex-row
   justify-between
-  px-5
+  px-3
   z-10
+  sm:flex
   sm:h-screen
   sm:w-64
   sm:flex-col
@@ -33,6 +33,6 @@
   @apply
   flex
   flex-col
-  gap-1
+  sm:gap-1
 }
 </style>
