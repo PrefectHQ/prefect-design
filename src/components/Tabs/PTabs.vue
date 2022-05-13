2498533303c7e12d317ffabd597f970e8ca8698a
Craig Harshbarger <pleek91@gmail.com>
2022-05-13 14:41:08 -0500

    Allow tabs to be disabled in the Tabs component

--- src/components/Tabs/PTabs.vue
+++ src/components/Tabs/PTabs.vue
@@ -1,80 +1,101 @@
 <template>
-  <section>
-    <ul class="p-tab" role="tablist" aria-label="Tab">
+  <section class="p-tabs">
+    <ul class="p-tabs__tabs" role="tablist" aria-label="Tab">
       <p-tab
-        v-for="(tab, index) in tabs"
-        :id="kebabCase(tab)"
-        :key="tab"
-        :active="selectedTab === tab"
-        :area-controls="`${kebabCase(tab)}-content`"
+        v-for="(tab, index) in innerTabs"
+        :id="kebabCase(tab.label)"
+        :key="tab.label"
+        :active="selectedTab === tab.label"
+        :disabled="tab.disabled"
+        :area-controls="`${kebabCase(tab.label)}-content`"
         @click="selectTab(tab)"
-        @keydown.enter.space="handleKeyDown(tab, $event)"
+        @keydown.enter.space="handleKeyDown(tab)"
       >
-        <slot :name="`${kebabCase(tab)}-heading`" v-bind="{ tab, index }">
-          {{ tab }}
+        <slot :name="`${kebabCase(tab.label)}-heading`" v-bind="scope(tab, index)">
+          {{ tab.label }}
         </slot>
       </p-tab>
     </ul>
 
-    <template v-for="tab in tabs" :key="tab">
+    <template v-for="tab in innerTabs" :key="tab">
       <section
-        v-if="selectedTab === tab"
-        :id="`${kebabCase(tab)}-content`"
+        v-if="selectedTab === tab.label"
+        :id="`${kebabCase(tab.label)}-content`"
         class="p-tabs__content"
         role="tabpanel"
-        :aria-labelledby="`${kebabCase(tab)}`"
+        :aria-labelledby="`${kebabCase(tab.label)}`"
       >
-        <slot :name="kebabCase(tab)" />
+        <slot :name="kebabCase(tab.label)" />
       </section>
     </template>
   </section>
 </template>
 
 <script lang="ts" setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, computed } from 'vue'
   import { PTab } from '@/components/Tab'
+  import { Tab, areTabs } from '@/types/tabs'
   import { kebabCase } from '@/utilities/strings'
 
   const selectedTab = ref()
 
   const props = defineProps<{
-    tabs: string[],
+    tabs: string[] | Tab[],
   }>()
 
   const emits = defineEmits<{
-    (event: 'tab', value: string): void,
+    (event: 'tab', value: string | Tab): void,
   }>()
 
-  function selectTab(tab: string): void {
-    selectedTab.value = tab
-    emits('tab', tab)
+  const innerTabs = computed<Tab[]>(() => {
+    if (areTabs(props.tabs)) {
+      return props.tabs
+    }
+
+    return props.tabs.map(label => ({ label }))
+  })
+
+  function selectTab(tab: Tab): void {
+    const value = areTabs(props.tabs) ? tab : tab.label
+    selectedTab.value = tab.label
+
+    emits('tab', value)
   }
 
-  function handleKeyDown(tab: string, event: Event): void {
-    const tabEl = event.target as HTMLElement
-    if (tabEl.classList.contains('p-tab__disabled')) {
+  function handleKeyDown(tab: Tab): void {
+    if (tab.disabled) {
       return
     }
+
     selectTab(tab)
   }
 
+  function scope(tab: Tab, index: number): { tab: string | Tab, index: number } {
+    if (areTabs(props.tabs)) {
+      return { tab, index }
+    }
+
+    return { tab: tab.label, index }
+  }
+
   onMounted(() => {
-    const [firstTab] = props.tabs
-    selectedTab.value = firstTab
+    const [firstTab] = innerTabs.value
+
+    selectedTab.value = firstTab.label
   })
 </script>
 
 <style>
-.p-tabs__content { @apply
-  mt-5
-}
-
-.p-tab { @apply
+.p-tabs__tabs { @apply
   border-b
   border-gray-200
   flex
   items-center
   -mb-px
   cursor-pointer
+}
+
+.p-tabs__content { @apply
+  mt-5
 }
 </style>
