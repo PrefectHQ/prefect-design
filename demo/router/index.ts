bdac141404e11498af3565e58e5c39a8eb7176e6
Marichka Offen <marichka@prefect.io>
2022-05-16 11:25:42 -0400

    remove unnecessary routes

--- demo/router/index.ts
+++ demo/router/index.ts
@@ -4,26 +4,6 @@ import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 
 const routeRecords: RouteRecordRaw[] = [
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import('../pages/ComponentsPage.vue'),
-  },
-  {
-    path: '/flows',
-    name: 'Flows',
-    component: () => import('../pages/ComponentsPage.vue'),
-  },
-  {
-    path: '/edit',
-    name: 'Edit',
-    component: () => import('../pages/ComponentsPage.vue'),
-  },
-  {
-    path: '/leave',
-    name: 'Leave',
-    component: () => import('../pages/ComponentsPage.vue'),
-  },
-  {
     name: 'components',
     path: '/components',
     component: () => import('../pages/ComponentsPage.vue'),
