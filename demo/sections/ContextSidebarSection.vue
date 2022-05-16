d51903b79e3e1b9734eb7470aef89231c2fba073
Marichka Offen <marichka@prefect.io>
2022-05-16 16:12:55 -0400

    Update PContextNavItem and demo for it

--- demo/sections/ContextSidebarSection.vue
+++ demo/sections/ContextSidebarSection.vue
@@ -1,15 +1,20 @@
 <template>
   <p-context-sidebar>
     <template #header>
-      <div class="flex flex-col py-3 sm:pt-0">
+      <div class="flex flex-col pt-3 sm:pt-0">
         <span class="font-normal text-sm mx-3">Prefect Design</span>
         <span class="font-bold text-base mx-3">UI Team Demo Collection</span>
       </div>
+      <hr class="border-none h-[1px] bg-slate-600 my-2 -mx-3">
     </template>
 
     <template #upper-links>
-      <p-context-nav-item title="Components" icon="PuzzleIcon" to="/components" />
-      <p-context-nav-item title="Layouts" icon="TemplateIcon" to="/layouts" />
+      <div class="flex flex-col gap-1">
+        <p-context-nav-item title="Components" icon="PuzzleIcon" to="/components" />
+        <hr class="border-none sm:h-0 sm:my-0 h-[1px] bg-slate-600 my-1 -mx-3">
+        <p-context-nav-item title="Layouts" icon="TemplateIcon" to="/layouts" />
+        <hr class="border-none sm:h-0 sm:my-0 h-[1px] bg-slate-600 my-1 -mx-3">
+      </div>
     </template>
 
     <template #bottom-links>
--- src/components/ContextNavItem/PContextNavItem.vue
+++ src/components/ContextNavItem/PContextNavItem.vue
@@ -1,22 +1,13 @@
 <template>
   <router-link
-    v-slot="{ href, navigate, isExactActive }"
+    exact-active-class="p-context-nav-item--active"
+    class="p-context-nav-item"
     :to="to"
-    custom
   >
-    <div class="p-context-nav-item">
-      <a
-        class="p-context-nav-item__link"
-        :href="href"
-        :class="{ 'p-context-nav-item--active': isExactActive }"
-        @click="navigate"
-      >
-        <template v-if="icon">
-          <p-icon :icon="icon" class="p-context-nav-item__icon" />
-        </template>
-        <span>{{ title }}</span>
-      </a>
-    </div>
+    <template v-if="icon">
+      <p-icon :icon="icon" class="p-context-nav-item__icon" />
+    </template>
+    <span>{{ title }}</span>
   </router-link>
 </template>
 
@@ -34,34 +25,21 @@
 <style>
 .p-context-nav-item {
   @apply
-  -mx-3
-  py-2
-  px-2
-  border-b
-  border-slate-600
-  first:border-t
-  sm:first:pt-1
-  sm:border-0
-  sm:py-0
-}
-
-.p-context-nav-item__link {
-  @apply
   flex
+  py-2.5
+  px-3
+  rounded-md
   items-center
+  transition
+  duration-200
+  ease-linear
   gap-3
   text-sm
   leading-5
   font-medium
-  py-2.5
-  px-2
-  rounded-md
   hover:bg-slate-600
   active:bg-slate-800
   focus:bg-white/25
-  transition
-  duration-200
-  ease-linear
 }
 
 .p-context-nav-item__icon{
