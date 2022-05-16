456c6fb9785574e9a70bc8360f1204b3c2e7ad61
Evan Sutherland <stackoverfloweth@gmail.com>
2022-05-16 09:54:00 -0500

    time-picker removed set button

--- src/components/DatePicker/PTimePicker.vue
+++ src/components/DatePicker/PTimePicker.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="p-time-picker">
-    <p-hour-picker v-model="time" class="p-time-picker__hours" />
-    <p-minute-picker v-model="time" class="p-time-picker__minutes" />
-    <p-meridiem-picker v-model="time" class="p-time-picker__meridiem" />
-
-    <div class="p-time-picker__actions">
-      <p-button secondary size="sm" @click="selectedDate = time">
-        Set
-      </p-button>
-    </div>
+    <p-hour-picker v-model="selectedDate" class="p-time-picker__hours" />
+    <p-minute-picker v-model="selectedDate" class="p-time-picker__minutes" />
+    <p-meridiem-picker v-model="selectedDate" class="p-time-picker__meridiem" />
   </div>
 </template>
 
 <script lang="ts" setup>
   import { startOfDay } from 'date-fns'
-  import { computed, ref } from 'vue'
-  import PButton from '@/components/Button'
+  import { computed } from 'vue'
   import PHourPicker from '@/components/DatePicker/PHourPicker.vue'
   import PMeridiemPicker from '@/components/DatePicker/PMeridiemPicker.vue'
   import PMinutePicker from '@/components/DatePicker/PMinutePicker.vue'
@@ -36,8 +29,6 @@
       emits('update:modelValue', value)
     },
   })
-
-  const time = ref(new Date(selectedDate.value))
 </script>
 
 <style>
@@ -51,14 +42,7 @@
 
 .p-time-picker__hours,
 .p-time-picker__minutes,
-.p-time-picker__meridiem,
-.p-time-picker__actions { @apply
+.p-time-picker__meridiem { @apply
   h-full
-}
-
-.p-time-picker__actions { @apply
-  flex
-  flex-col
-  justify-center
 }
 </style>
