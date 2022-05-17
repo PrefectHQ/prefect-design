c58e72b8a05b0ec13a1d951822b2a30fea7585a3
Evan Sutherland <stackoverfloweth@gmail.com>
2022-05-17 11:51:56 -0500

    extracted scrolling-picker base component

--- src/components/DatePicker/PYearPicker.vue
+++ src/components/DatePicker/PYearPicker.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="p-year-picker">
-    <div ref="topElement" class="p-year-picker__observer" data-target="top" />
-    <template v-for="(year, index) in years" :key="year">
-      <button type="button" class="p-year-picker__year" :tabindex="index" :class="classes.year(year)" @click="updateSelectedDate(year)">
-        <span ref="yearElements" :data-year="year">{{ year }}</span>
-      </button>
-    </template>
-    <div ref="bottomElement" class="p-year-picker__observer" data-target="bottom" />
-  </div>
+  <scrolling-picker
+    ref="scrollingPicker"
+    :model-value="selectedYear"
+    :options="years"
+    @update:model-value="updateSelectedDate"
+    @scroll-top="handleScrollTop"
+    @scroll-bottom="handleScrollBottom"
+  />
 </template>
 
 <script lang="ts" setup>
   import { setYear, startOfDay } from 'date-fns'
-  import { computed, onMounted, onUnmounted, ref, nextTick } from 'vue'
+  import { computed, ref, nextTick } from 'vue'
+  import ScrollingPicker from '@/components/DatePicker/ScrollingPicker.vue'
+  import { SelectModelValue, SelectOption } from '@/types/selectOption'
 
   const props = defineProps<{
     modelValue: Date | null | undefined,
@@ -35,109 +36,30 @@
 
   const viewingYear = ref(selectedYear.value)
   const viewingCount = 20
-  const yearElements = ref<HTMLElement[]>([])
-  const topElement = ref<HTMLElement>()
-  const bottomElement = ref<HTMLElement>()
+  const scrollingPicker = ref<typeof ScrollingPicker>()
 
   const years = computed(() => {
-    const values: number[] = []
+    const values: SelectOption[] = []
     const minYear = viewingYear.value - viewingCount
     const maxYear = viewingYear.value + viewingCount
 
     for (let year=minYear; year<maxYear; year++) {
-      values.push(year)
+      values.push({ label: year.toString(), value: year })
     }
 
     return values
   })
 
-  const classes = computed(() => ({
-    year:(year: number) => ({
-      'p-year-picker__year--selected': selectedDate.value.getFullYear() === year,
-    }),
-  }))
-
-  function updateSelectedDate(year: number): void {
-    selectedDate.value = setYear(selectedDate.value, year)
+  function updateSelectedDate(value: SelectModelValue): void {
+    selectedDate.value = setYear(selectedDate.value, value as number)
   }
 
-  function handleIntersection([entry]: IntersectionObserverEntry[]): void {
-    if (entry.isIntersecting) {
-      const target = entry.target as HTMLElement
-
-      if (target.dataset.target === 'top') {
-        viewingYear.value = viewingYear.value - viewingCount
-        nextTick(() => scrollToYear(viewingYear.value))
-      } else {
-        viewingYear.value = viewingYear.value + viewingCount
-      }
-    }
+  function handleScrollTop(): void {
+    viewingYear.value = viewingYear.value - viewingCount
+    nextTick(() => scrollingPicker.value!.scrollToOption(viewingYear.value))
   }
 
-  function scrollToYear(year: number): HTMLElement | undefined {
-    const element = yearElements.value.find(node => node.dataset.year === year.toString())
-
-    if (element) {
-      element.scrollIntoView({ block: 'center' })
-    }
-
-    return element
+  function handleScrollBottom(): void {
+    viewingYear.value = viewingYear.value + viewingCount
   }
-
-  let observer: IntersectionObserver | null = null
-
-  onMounted(() => {
-    observer = new IntersectionObserver(handleIntersection)
-
-    const element = scrollToYear(selectedYear.value)
-
-    if (topElement.value && bottomElement.value) {
-      observer.observe(topElement.value)
-      observer.observe(bottomElement.value)
-    }
-
-    if (element) {
-      element.parentElement?.focus()
-    }
-  })
-
-  onUnmounted(() => observer?.disconnect())
 </script>
-
-<style>
-.p-year-picker { @apply
-  flex
-  flex-col
-  gap-2
-  snap-y
-  px-4
-  py-3
-  overflow-y-auto
-}
-
-.p-year-picker__year { @apply
-  text-center
-  snap-center
-  py-1
-  px-2
-  text-sm
-  cursor-pointer
-  rounded
-  hover:bg-gray-100
-  focus:ring-prefect-600
-  focus:outline-none
-  focus:ring-2
-  focus:ring-offset-2
-}
-
-.p-year-picker__year--selected { @apply
-  text-white
-  bg-prefect-600
-  hover:bg-prefect-800
-}
-
-.p-year-picker__observer { @apply
-  snap-center
-  p-1
-}
-</style>
---
+++ src/components/DatePicker/ScrollingPicker.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="scrolling-picker">
+    <div ref="topElement" class="scrolling-picker__observer" data-target="top" />
+    <template v-for="(option, index) in selectOptions" :key="option.value">
+      <button
+        type="button"
+        class="scrolling-picker__option"
+        :tabindex="index"
+        :class="classes.option(option)"
+        @click="internalValue = option.value"
+      >
+        <span ref="optionElements" :data-target="option.value">{{ option.label }}</span>
+      </button>
+    </template>
+    <div ref="bottomElement" class="scrolling-picker__observer" data-target="bottom" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed, onMounted, onUnmounted, ref } from 'vue'
+  import { isSelectOption, SelectModelValue, SelectOption } from '@/types/selectOption'
+
+  const props = defineProps<{
+    modelValue: string | number | null | undefined,
+    options: (string | number | SelectOption)[],
+  }>()
+
+  const emits = defineEmits<{
+    (event: 'update:modelValue', value: SelectModelValue): void,
+    (event: 'scroll-top' | 'scroll-bottom'): void,
+  }>()
+
+  defineExpose({ scrollToOption })
+
+  const internalValue = computed({
+    get() {
+      return props.modelValue ?? null
+    },
+    set(value: SelectModelValue) {
+      emits('update:modelValue', value)
+    },
+  })
+
+  const selectOptions = computed<SelectOption[]>(() => props.options.map(option => {
+    if (isSelectOption(option)) {
+      return option
+    }
+
+    return { label: option.toLocaleString(), value: option }
+  }))
+
+  const optionElements = ref<HTMLElement[]>([])
+  const topElement = ref<HTMLElement>()
+  const bottomElement = ref<HTMLElement>()
+
+  const classes = computed(() => ({
+    option:(option: SelectOption) => ({
+      'scrolling-picker__option--selected': option.value === internalValue.value,
+    }),
+  }))
+
+  function handleIntersection([entry]: IntersectionObserverEntry[]): void {
+    if (entry.isIntersecting) {
+      const target = entry.target as HTMLElement
+
+      if (target.dataset.target === 'top') {
+        emits('scroll-top')
+      } else {
+        emits('scroll-bottom')
+      }
+    }
+  }
+
+  function scrollToOption(value: SelectModelValue): HTMLElement | undefined {
+    const element = optionElements.value.find(node => node.dataset.target === value?.toString())
+
+    if (element) {
+      element.scrollIntoView({ block: 'center' })
+    }
+
+    return element
+  }
+
+  let observer: IntersectionObserver | null = null
+
+  onMounted(() => {
+    observer = new IntersectionObserver(handleIntersection)
+
+    const element = scrollToOption(internalValue.value)
+
+    if (topElement.value && bottomElement.value) {
+      observer.observe(topElement.value)
+      observer.observe(bottomElement.value)
+    }
+
+    if (element) {
+      element.parentElement?.focus()
+    }
+  })
+
+  onUnmounted(() => observer?.disconnect())
+</script>
+
+<style>
+.scrolling-picker { @apply
+  flex
+  flex-col
+  gap-2
+  px-4
+  py-3
+  overflow-y-auto
+}
+
+.scrolling-picker__option { @apply
+  text-center
+  py-1
+  px-2
+  text-sm
+  cursor-pointer
+  rounded
+  hover:bg-gray-100
+  focus:ring-prefect-600
+  focus:outline-none
+  focus:ring-2
+  focus:ring-offset-2
+}
+
+.scrolling-picker__option--selected { @apply
+  text-white
+  bg-prefect-600
+  hover:bg-prefect-800
+}
+
+.scrolling-picker__observer { @apply
+  p-1
+}
+</style>
