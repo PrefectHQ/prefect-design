57e603553572baa2e4b72356f3999c0fcd4f4f85
Marichka Offen <marichka@prefect.io>
2022-05-18 14:16:27 -0400

    Update PDefaultLayout layout

--- src/layouts/PDefaultLayout/PDefaultLayout.vue
+++ src/layouts/PDefaultLayout/PDefaultLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-default-layout">
     <div class="p-default-layout__header">
-      <slot name="header" />
+      <slot name="header" class="p-default-layout__header" />
     </div>
     <div class="p-default-layout__content">
       <slot name="content" />
@@ -12,12 +12,12 @@
 <style>
 .p-default-layout {
   @apply
+  p-8
+  grid
+  grid-cols-1
+  grid-rows-[repeat(2,_auto)]
+  gap-8
   h-full
   w-full
-  p-8
-}
-.p-default-layout__header {
-  @apply
-  pb-8
 }
 </style>
