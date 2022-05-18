75eee0b814806b6847dd2a07f644c444bc33b64a
Marichka Offen <marichka@prefect.io>
2022-05-18 16:41:42 -0400

    Set up demo for full layout component

---
+++ demo/sections/FullLayoutSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <Section heading="Full Layout">
+    <resizable-section class="h-[200px]">
+      <p-full-layout class="bg-slate-100">
+        <template #header>
+          <div class="bg-slate-300 p-2">
+            <p>Header</p>
+            <p>Header</p>
+          </div>
+        </template>
+        <template #content>
+          <div>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+          </div>
+        </template>
+      </p-full-layout>
+    </resizable-section>
+  </Section>
+</template>
+
+<script lang="ts" setup>
+  import ResizableSection from '../components/ResizableSection.vue'
+  import Section from '../components/Section.vue'
+</script>
