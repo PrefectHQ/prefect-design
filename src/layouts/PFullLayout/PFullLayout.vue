361a0c42f2d7bcf7b1c16cb4ffeda198168a26cb
Marichka Offen <marichka@prefect.io>
2022-05-18 16:41:23 -0400

    Create Full Layout component

--- src/layouts/PFullLayout/PFullLayout.vue
+++ src/layouts/PFullLayout/PFullLayout.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    Full Layout
+  <div class="p-full-layout">
+    <div class="p-full-layout__header">
+      <slot name="header" class="p-full-layout__header" />
+    </div>
+
+    <slot name="content" />
   </div>
 </template>
+
+<style>
+.p-full-layout {
+   @apply
+  w-screen
+  h-screen
+}
+.p-full-layout__header {
+  @apply
+  absolute
+  top-8
+  left-8
+  right-8
+}
+</style>
