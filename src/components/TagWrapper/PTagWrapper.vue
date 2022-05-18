483851d96c7341592548ae07f50d498fcbe5010c
nicholas <znicholasbrown@gmail.com>
2022-05-18 14:30:59 -0400

    Rewrite tag wrapper to use bbox

--- src/components/TagWrapper/PTagWrapper.vue
+++ src/components/TagWrapper/PTagWrapper.vue
@@ -2,17 +2,19 @@
   <div ref="container" class="p-tag-wrapper">
     <div class="p-tag-wrapper__tag-container" :class="classes.tagContainer">
       <slot>
-        <template v-for="tag in tags">
-          <slot name="tag" :tag="tag">
-            <p-tag class="p-tag-wrapper__tag" :class="classes.tag">
-              {{ tag }}
-            </p-tag>
-          </slot>
+        <template v-for="tag in tags" :key="tag">
+          <div class="p-tag-wrapper__tag" :class="classes.tag">
+            <slot name="tag" :tag="tag">
+              <p-tag>
+                {{ tag }}
+              </p-tag>
+            </slot>
+          </div>
         </template>
       </slot>
 
 
-      <div v-show="hasOverflowChildren" ref="overflowTag" class="p-tag-wrapper__tag-overflow">
+      <div ref="overflowTag" class="p-tag-wrapper__tag-overflow" :class="classes.overflowTag">
         <slot name="overflow-tags" :overflowed-children="overflowChildren">
           <p-tag>
             +{{ overflowChildren }}
@@ -32,69 +34,76 @@
     justify?: 'left' | 'center' | 'right',
   }>()
   const container: Ref<HTMLDivElement | undefined> = ref()
-  const overflowChildren = ref(1)
+  const ready = ref(false)
+  const overflowChildren = ref(0)
   const overflowTag = ref()
 
   const classes = computed(() => {
     return {
+      overflowTag: { 'p-tag-wrapper__tag--hidden': !hasOverflowChildren.value },
       tag: [`p-tag-wrapper__tag--${props.justify ?? 'left'}`],
       tagContainer:
-        [`p-tag-wrapper__tag-container--${props.justify ?? 'left'}`],
+        [
+          `p-tag-wrapper__tag-container--${props.justify ?? 'left'}`,
+          { 'p-tag-wrapper__tag-container--invisible': !ready.value },
+        ],
 
     }
   })
 
   let resizeObserver: ResizeObserver | null = null
   const hasOverflowChildren = computed(() => overflowChildren.value > 0)
-  function setChildIsVisible(child: HTMLElement): boolean {
-    const containerWidth = container.value?.offsetWidth ?? 0
 
+  const calculateOverflow = (entries: ResizeObserverEntry[]): void => {
+    const children = Array.from(
+      entries[0]?.target?.querySelector('.p-tag-wrapper__tag-container')?.children ?? [])
+      .filter(child => !child.classList.contains('p-tag-wrapper__tag-overflow'),
+      ) as HTMLElement[]
+
+    const containerBoundingBox = container.value!.getBoundingClientRect()
     const overflowTagWidth = overflowTag.value.offsetWidth ?? 55
 
-    if (child.classList.contains('p-tag-wrapper__tag--hidden')) {
+    let overflowed = false
+    let overflowLen = children.length
+    let tagsWidth = 0
+    let largestChildHeight = overflowTag.value.offsetHeight
+
+    for (const child of children) {
       child.classList.add('p-tag-wrapper__tag--invisible')
       child.classList.remove('p-tag-wrapper__tag--hidden')
+
+      if (overflowed) {
+        child.classList.add('p-tag-wrapper__tag--hidden')
+        child.classList.remove('p-tag-wrapper__tag--invisible')
+      } else {
+        const boundingBox = child.getBoundingClientRect()
+        overflowed = tagsWidth + boundingBox.width  >= containerBoundingBox.width - overflowTagWidth
+
+        if (overflowed) {
+          child.classList.add('p-tag-wrapper__tag--hidden')
+          child.classList.remove('p-tag-wrapper__tag--invisible')
+        } else {
+          child.classList.remove('p-tag-wrapper__tag--invisible')
+          child.classList.remove('p-tag-wrapper__tag--hidden')
+
+          tagsWidth = tagsWidth + boundingBox.width
+          largestChildHeight = Math.max(largestChildHeight, boundingBox.height)
+          overflowLen--
+        }
+      }
     }
 
-    const childOverflowsContainer = child.offsetLeft + child.offsetWidth  > containerWidth - overflowTagWidth
-
-
-    if (childOverflowsContainer) {
-      child.classList.add('p-tag-wrapper__tag--hidden')
-    } else {
-      child.classList.remove('p-tag-wrapper__tag--hidden')
-    }
-    child.classList.remove('p-tag-wrapper__tag--invisible')
-
-    return childOverflowsContainer
-  }
-
-  function updateOverflownChildren(entries: ResizeObserverEntry[]): void {
-    const children = Array.from(entries[0]?.target?.querySelector('.p-tag-wrapper__tag-container')?.children ?? []) as HTMLElement[]
-    let largestChildHeight = 0
-    overflowChildren.value = children.reduce((sum, entry) => {
-      largestChildHeight = Math.max(largestChildHeight, entry.offsetHeight)
-      return sum + getChildOverflow([entry])
-    }, 0)
+    overflowChildren.value = overflowLen
 
     if (container.value) {
       container.value.style.height = `${largestChildHeight}px`
     }
-  }
 
-  function getChildOverflow(children: HTMLElement[]): number {
-    return children
-      .filter(child => !child.classList.contains('p-tag-wrapper__tag-overflow'))
-      .reduce((childSum, child) => {
-        if (setChildIsVisible(child)) {
-          childSum++
-        }
-        return childSum
-      }, 0)
+    ready.value = true
   }
 
   function createObserver(): void {
-    resizeObserver = new ResizeObserver(updateOverflownChildren)
+    resizeObserver = new ResizeObserver(calculateOverflow)
 
     if (container.value) {
       resizeObserver.observe(container.value)
@@ -126,16 +135,20 @@
   visibility: hidden !important;
 }
 
+.p-tag-wrapper__tag { @apply
+  inline-block;
+}
+
 .p-tag-wrapper__tag--right { @apply
-  ml-1;
+  pl-1;
 }
 
 .p-tag-wrapper__tag--center { @apply
-  mx-[0.125rem];
+  px-[0.125rem];
 }
 
 .p-tag-wrapper__tag--left { @apply
-  mr-1;
+  pr-1;
 }
 
 .p-tag-wrapper__tag-overflow {
@@ -148,6 +161,10 @@
   top-0
   w-full
   h-full;
+}
+
+.p-tag-wrapper__tag-container--invisible {
+  visibility: hidden !important;
 }
 
 .p-tag-wrapper__tag-container--right { @apply
