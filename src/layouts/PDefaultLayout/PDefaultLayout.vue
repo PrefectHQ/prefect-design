07fefd6a67efcff621ecc314c3754b9a8b0b99c1
Marichka Offen <marichka@prefect.io>
2022-05-18 12:06:35 -0400

    Build PDefaultLayout

--- src/layouts/PDefaultLayout/PDefaultLayout.vue
+++ src/layouts/PDefaultLayout/PDefaultLayout.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    Default Layout
+  <div class="p-default-layout">
+    <div class="p-default-layout__header">
+      <slot name="header" />
+    </div>
+    <div class="p-default-layout__content">
+      <slot name="content" />
+    </div>
   </div>
 </template>
+
+<style>
+.p-default-layout {
+  @apply
+  h-full
+  w-full
+  p-8
+}
+.p-default-layout__header {
+  @apply
+  pb-8
+}
+</style>
