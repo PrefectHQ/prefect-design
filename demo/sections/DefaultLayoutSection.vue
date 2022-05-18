f1a05ca23e1c5ec6fae391314077581093b45363
Marichka Offen <marichka@prefect.io>
2022-05-18 12:07:03 -0400

    Build default layout section for demo

---
+++ demo/sections/DefaultLayoutSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <resizable-section>
+    <p-default-layout class="bg-slate-100">
+      <template #header>
+        <div class="bg-slate-300 h-20 p-4">
+          Header
+        </div>
+      </template>
+      <template #content>
+        <div class="bg-slate-300 h-36 p-4">
+          Content
+        </div>
+      </template>
+    </p-default-layout>
+  </resizable-section>
+</template>
+
+<script lang="ts" setup>
+  import ResizableSection from '../components/ResizableSection.vue'
+</script>
