2fe7c47e86eed8f8b7c105dfd857253b96e8e47a
Evan Sutherland <stackoverfloweth@gmail.com>
2022-05-27 09:14:00 -0500

    updated states to match vee-validate field-meta

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,17 +1,10 @@
 /* eslint-disable line-comment-position */
-// https://codesandbox.io/s/j2v2sf
 
 export type State = {
-  valid: boolean, // the field is valid.
-  invalid: boolean, // the field is invalid.
-  changed: boolean, // the field value has been changed from it's initial value.
-  touched: boolean, // the field has been blurred at least once.
-  untouched: boolean, // the field has never been blurred.
-  pristine: boolean, // the field value has never been modified.
-  dirty: boolean, // the field value has been modified.
-  pending: boolean, // validation is in progress.
-  required: boolean, // the field is required.
-  validated: boolean, // the field has been validated at least once.
-  passed: boolean, // the field has been validated and is valid.
-  failed: boolean, // the field has been validated and is invalid.
+  touched: boolean, // if the field has been blurred (via handleBlur)
+  pending: boolean, // if validation is in progress
+  dirty: boolean, // if the field has been manipulated (via handleChange)
+  valid: boolean, // if the field doesn't have any errors
+  validated: boolean, // if the field has been validated
+  initialValue?: unknown, // the field's initial value
 }
