cf27405df73ea4fbc92562bfb8c09af4c88447eb
nicholas <znicholasbrown@gmail.com>
2022-06-02 18:23:58 -0400

    Add a loading state to p-toggle

--- demo/sections/TogglesSection.vue
+++ demo/sections/TogglesSection.vue
@@ -5,20 +5,55 @@
         <span class="text-sm font-medium text-gray-900">{{ toggleOne }}</span>
       </template>
     </p-toggle>
+
     <p-toggle v-model="toggleTwo" class="py-2" disabled>
       <template #append>
         <span class="text-sm font-medium text-gray-900">disabled</span>
+      </template>
+    </p-toggle>
+
+    <p-toggle v-model="toggleThree" loading class="py-2">
+      <template #append>
+        <span class="text-sm font-medium text-gray-900">Loading</span>
+      </template>
+    </p-toggle>
+
+    <p-toggle v-model="toggleFour" :loading="toggleFourLoading" class="py-2">
+      <template #append>
+        <span class="text-sm font-medium text-gray-900">Async state</span>
       </template>
     </p-toggle>
   </Section>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
   import Section from '../components/Section.vue'
   import PToggle from '@/components/Toggle/PToggle.vue'
 
   const toggleOne = ref(false)
   const toggleTwo = ref(false)
+  const toggleThree = ref(false)
+
+  const toggleFourApiValue = ref(false)
+  const toggleFourLoading = ref(false)
+
+  const toggleFour = computed({
+    get() {
+      return toggleFourApiValue.value
+    },
+    set(val: boolean) {
+      if (toggleFourLoading.value) {
+        return
+      }
+
+      toggleFourLoading.value = true
+
+      setTimeout(() => {
+        toggleFourApiValue.value = val
+        toggleFourLoading.value = false
+      }, 2000)
+    },
+  })
 </script>
 
--- src/components/Toggle/PToggle.vue
+++ src/components/Toggle/PToggle.vue
@@ -1,7 +1,15 @@
 <template>
   <SwitchGroup as="div" class="p-toggle">
-    <Switch v-model="value" class="p-toggle__switch" :class="classes.toggle" :disabled="disabled">
-      <span aria-hidden="true" class="p-toggle__switch-aria" :class="classes.translate" />
+    <Switch v-model="value" class="p-toggle__switch" :class="classes.toggle" :disabled="disabled || loading">
+      <span aria-hidden="true" class="p-toggle__switch-aria" :class="classes.translate">
+        <div v-if="loading" class="p-toggle__switch-loader">
+          <div />
+          <div />
+          <div />
+          <div />
+
+        </div>
+      </span>
     </Switch>
     <SwitchLabel v-if="$slots.append" as="span" class="ml-3">
       <slot name="append" />
@@ -16,6 +24,7 @@
   const props = defineProps<{
     modelValue: boolean,
     disabled?: boolean,
+    loading?: boolean,
   }>()
 
   const emits = defineEmits<{
@@ -38,8 +47,9 @@
       'translate-x-0': !props.modelValue,
     },
     toggle: {
-      'p-toggle__switch-disabled': props.disabled,
-      'p-toggle__switch-enabled': props.modelValue,
+      'p-toggle__switch--disabled': props.disabled,
+      'p-toggle__switch--enabled': props.modelValue,
+      'p-toggle__switch--loading': props.loading,
     },
   }))
 </script>
@@ -49,6 +59,7 @@
   flex
   items-center
 }
+
 .p-toggle__switch { @apply
   relative
   inline-flex
@@ -67,6 +78,7 @@
   focus:ring-prefect-500
   bg-gray-200
 }
+
 .p-toggle__switch-aria { @apply
   pointer-events-none
   inline-block
@@ -74,6 +86,7 @@
   w-5
   rounded-full
   bg-white
+  overflow-hidden
   shadow
   transform
   ring-0
@@ -81,11 +94,63 @@
   ease-in-out
   duration-200
 }
-.p-toggle__switch-disabled { @apply
+
+.p-toggle__switch--disabled { @apply
   cursor-not-allowed
   opacity-50
 }
-.p-toggle__switch-enabled { @apply
+
+.p-toggle__switch--loading { @apply
+  cursor-wait
+}
+
+.p-toggle__switch--enabled { @apply
   bg-prefect-500
+}
+
+.p-toggle__switch-loader {
+  @apply
+  absolute
+  w-full
+  h-full
+}
+
+.p-toggle__switch-loader div {
+  animation: loading 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+
+  @apply
+  absolute
+  w-4
+  h-4
+  border-2
+  border-primary
+  border-r-transparent
+  border-b-transparent
+  border-l-transparent
+  rounded-[50%]
+  top-[50%]
+  left-[50%]
+}
+
+
+.p-toggle__switch-loader div:nth-child(1) {
+  animation-delay: -450ms;
+}
+
+.p-toggle__switch-loader div:nth-child(2) {
+  animation-delay: -300ms;
+}
+
+.p-toggle__switch-loader div:nth-child(3) {
+  animation-delay: -150ms;
+}
+
+@keyframes loading {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
 }
 </style>
