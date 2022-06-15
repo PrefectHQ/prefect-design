12b34284d915506ff87155fa5ad54df900efb66f
Evan Sutherland <stackoverfloweth@gmail.com>
2022-06-15 12:25:04 -0500

    setting default icon size to 14

--- src/components/EmptyState/PEmptyState.vue
+++ src/components/EmptyState/PEmptyState.vue
@@ -6,7 +6,7 @@
     <div
       class="p-empty-state__text"
     >
-      <div class="p-empty-state__icon ">
+      <div class="p-empty-state__icon">
         <slot name="icon" />
       </div>
 
@@ -25,48 +25,53 @@
 </template>
 
 
-  <style>
-  .p-empty-state { @apply
-    border
-    border-gray-300
-    bg-white
-    rounded-lg
-    shadow-sm
-  }
+<style>
+.p-empty-state { @apply
+  border
+  border-gray-300
+  bg-white
+  rounded-lg
+  shadow-sm
+}
 
-  .p-empty-state__corner-label { @apply
-    mt-2
-    text-right
-  }
+.p-empty-state__corner-label { @apply
+  mt-2
+  text-right
+}
 
-  .p-empty-state__text { @apply
-    text-center
-    p-16
-  }
+.p-empty-state__text { @apply
+  text-center
+  p-16
+}
 
-  .p-empty-state__icon { @apply
-    text-gray-400
-    flex
-    items-center
-    justify-center
-    gap-x-4
-  }
+.p-empty-state__icon { @apply
+  text-gray-400
+  flex
+  items-center
+  justify-center
+  gap-x-4
+}
 
-  .p-empty-state__heading { @apply
-    mt-9
-    text-gray-900
-    text-2xl
-    font-bold
-  }
+.p-empty-state__icon .p-icon { @apply
+  h-14
+  w-14
+}
 
-  .p-empty-state__description { @apply
-    text-gray-500
-    text-base
-    font-normal
-    mt-2
-  }
+.p-empty-state__heading { @apply
+  mt-9
+  text-gray-900
+  text-2xl
+  font-bold
+}
 
-  .p-empty-state__actions { @apply
-    mt-6
-  }
-  </style>
+.p-empty-state__description { @apply
+  text-gray-500
+  text-base
+  font-normal
+  mt-2
+}
+
+.p-empty-state__actions { @apply
+  mt-6
+}
+</style>
