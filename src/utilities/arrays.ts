f4267e9a6349f463400dd3fe42699e48639f3217
Evan Sutherland <stackoverfloweth@gmail.com>
2022-07-07 10:30:36 -0500

    Update src/utilities/arrays.ts
    Co-authored-by: pleek91 <craig@prefect.io>

--- src/utilities/arrays.ts
+++ src/utilities/arrays.ts
@@ -2,12 +2,12 @@ export function isStrings(array: unknown[]): array is string[] {
   return array.every(value => typeof value === 'string')
 }
 
-export function reduceToRecord<S, T>(array: S[], key: (item: S) => string, value: (item: S) => T): Record<string, T> {
-  return array.reduce<Record<string, T>>((reduced, item) => {
+export function reduceToRecord<S, K extends string | number | symbol, V>(array: S[], key: (item: S) => K, value: (item: S) => V): Record<K, V> {
+  return array.reduce((reduced, item) => {
     reduced[key(item)] = value(item)
 
     return reduced
-  }, {})
+  }, {} as Record<K, V>)
 }
 
 export function reduceToMap<S, K, V>(array: S[], key: (item: S) => K, value: (item: S) => V): Map<K, V> {
