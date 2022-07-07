7bde7728f7c45e2ad0eda31f2b9cbcda39296034
Evan Sutherland <stackoverfloweth@gmail.com>
2022-07-07 09:44:50 -0500

    added reduceToRecord and reduceToMap utilities

--- src/utilities/arrays.ts
+++ src/utilities/arrays.ts
@@ -1,3 +1,19 @@
 export function isStrings(array: unknown[]): array is string[] {
   return array.every(value => typeof value === 'string')
 }
+
+export function reduceToRecord<S, T>(array: S[], key: (item: S) => string, value: (item: S) => T): Record<string, T> {
+  return array.reduce<Record<string, T>>((reduced, item) => {
+    reduced[key(item)] = value(item)
+
+    return reduced
+  }, {})
+}
+
+export function reduceToMap<S, T>(array: S[], key: (item: S) => string, value: (item: S) => T): Map<string, T> {
+  return array.reduce<Map<string, T>>((reduced, item) => {
+    reduced.set(key(item), value(item))
+
+    return reduced
+  }, new Map())
+}
