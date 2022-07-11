7e852a9be88704837eb1671f6d75a643df004f31
Craig Harshbarger <pleek91@gmail.com>
2022-07-11 14:06:23 -0500

    Accept components as toast messages

---
+++ demo/components/ToastMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="toast-message">
+    Hello World! Here's a
+    <p-link href="#">
+      link
+    </p-link>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import PLink from '@/components/Link/PLink.vue'
+</script>
