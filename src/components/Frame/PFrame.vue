e54cb98f547f3777105610635453cd0d1591202d
Evan Sutherland <stackoverfloweth@gmail.com>
2022-09-07 15:01:38 -0500

    added frame component

---
+++ src/components/Frame/PFrame.vue
@@ -0,0 +1,45 @@
+<template>
+  <iframe ref="frame" srcdoc="<body></body>" :height="height" v-bind="$attrs" @load="onLoad" />
+  <teleport v-if="content" :to="content">
+    <slot />
+  </teleport>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'PFrame',
+    expose: [],
+    inheritAttrs: false,
+  }
+</script>
+
+<script lang="ts" setup>
+  import { useElementRect } from '@/../../vue-compositions'
+  import { ref } from 'vue'
+
+  const frame = ref<HTMLIFrameElement>()
+  const content = ref<HTMLElement>(document.createElement('div'))
+
+  const { height } = useElementRect(content)
+
+  function addStyleAndLinks(head: HTMLHeadElement): void {
+    const styles = Array.from(document.getElementsByTagName('style'))
+    const links = Array.from(document.getElementsByTagName('link'))
+
+    for (const el of styles.concat(links)) {
+      const styleElement = el.cloneNode(true)
+      head.appendChild(styleElement)
+    }
+  }
+
+  function onLoad(): void {
+    if (!frame.value?.contentDocument) {
+      return
+    }
+
+    const [head] = frame.value.contentDocument.getElementsByTagName('head')
+    addStyleAndLinks(head)
+    const [body] = frame.value.contentDocument.getElementsByTagName('body')
+    body.appendChild(content.value)
+  }
+</script>
