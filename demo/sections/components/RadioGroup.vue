82e0141c30c1c888b360b6c95efdbecb779d7a14
Evan Sutherland <stackoverfloweth@gmail.com>
2022-09-19 22:08:28 -0500

    added RadioGroup demo

---
+++ demo/sections/components/RadioGroup.vue
@@ -0,0 +1,57 @@
+<template>
+  <ComponentPage
+    title="Radio Group"
+    :demos="[
+      { title: 'Radio Group' },
+      { title: 'Using Label Slot' },
+    ]"
+  >
+    <template #description>
+      This is where we add a short description of <p-code>p-radio-group</p-code>. Describe the components intent, not hyper specific documentation that belongs on vitepress page.
+
+      <DemoState v-model:state="exampleState" v-model:disabled="disabled" />
+    </template>
+
+    <template #radio-group>
+      <div class="radio-group__demo">
+        <p-radio-group v-model="value" :options="options" :state="exampleState" :disabled="disabled" />
+
+        <p-code>value:{{ JSON.stringify(value) }}</p-code>
+      </div>
+    </template>
+
+    <template #using-label-slot>
+      <div class="radio-group__demo">
+        <p-radio-group v-model="slotValue" :options="options" :state="exampleState" :disabled="disabled">
+          <template #label="{ option }">
+            <p-code>{{ option.label }}</p-code>
+          </template>
+        </p-radio-group>
+
+        <p-code>value:{{ JSON.stringify(slotValue) }}</p-code>
+      </div>
+    </template>
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import { State } from '@/types'
+  import { ref } from 'vue'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import DemoState from '@/demo/components/DemoState.vue'
+
+  const exampleState = ref<State>()
+  const disabled = ref(false)
+
+  const value = ref(null)
+  const slotValue = ref(null)
+  const options = ['first', 'second', 'third']
+</script>
+
+<style>
+.radio-group__demo { @apply
+  flex
+  flex-col
+  gap-4
+}
+</style>
