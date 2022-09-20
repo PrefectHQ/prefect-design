b7c97ed1e20ead9de2472dd6a3c050f0965bcee9
Evan Sutherland <stackoverfloweth@gmail.com>
2022-09-19 22:08:01 -0500

    moved example-state into component for separating input components from form demo

---
+++ demo/components/DemoState.vue
@@ -0,0 +1,52 @@
+<template>
+  <p-label label="Validation State" class="demo-state">
+    <p-checkbox v-model="state.valid" label="valid" />
+    <p-checkbox v-model="state.validated" label="validated" />
+    <p-checkbox v-model="state.pending" label="pending" />
+    <p-checkbox v-model="internalDisabled" label="disabled" />
+  </p-label>
+</template>
+
+<script lang="ts" setup>
+  import { State } from '@/types'
+  import { computed, reactive, watch } from 'vue'
+
+  const props = defineProps<{
+    state: State | null | undefined,
+    disabled: boolean | null | undefined,
+  }>()
+
+  const emits = defineEmits<{
+    (event: 'update:state', value: State): void,
+    (event: 'update:disabled', value: boolean): void,
+  }>()
+
+  const state = reactive<State>({
+    valid: props.state?.valid ?? true,
+    validated: props.state?.validated ?? false,
+    pending: props.state?.pending ?? false,
+    dirty: props.state?.dirty ?? false,
+    touched: props.state?.touched ?? false,
+  })
+
+  const internalDisabled = computed({
+    get() {
+      return props.disabled ?? false
+    },
+    set(value: boolean) {
+      emits('update:disabled', value)
+    },
+  })
+
+  watch(state, value => emits('update:state', value))
+</script>
+
+<style>
+.demo-state { @apply
+  max-w-fit
+  px-8
+  py-4
+  my-4
+  bg-prefect-100
+}
+</style>
