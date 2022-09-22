b39fc5dfe8ef2b6d31eeede94f1b14b76e0b793f
Marichka Offen <marichka@prefect.io>
2022-09-22 11:01:48 -0400

    Create v-premoji directive

--- demo/main.ts
+++ demo/main.ts
@@ -1,8 +1,6 @@
-import '../src/styles/index.css'
-import 'prismjs/themes/prism-okaidia.css'
-
+import { convertToPremoji } from '@/compositions/usePremoji'
 import { plugin } from '@/index'
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 
@@ -10,6 +8,15 @@ import { router } from './router'
 const app = createApp(App)
 app.use(plugin)
 app.use(router)
+
+app.directive('premoji', (el) => {
+  const elementNode = el.firstChild.textContent
+  const parentNode = ref(el.parentNode)
+
+  if (parentNode.value) {
+    parentNode.value.innerHTML = convertToPremoji(elementNode)
+  }
+})
 
 app.config.performance = true
 
--- demo/sections/components/Premoji.vue
+++ demo/sections/components/Premoji.vue
@@ -1,12 +1,11 @@
 <template>
   <ComponentPage title="Premojis" :demos="[{ title: 'Premojis' }]">
     <template #premojis>
-      <span v-html="convertToPremoji(':google:-random-words:abc123: :github:')" />
+      <span v-premoji>:google:-random-words:abc123: :github:</span>
     </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
-  import { convertToPremoji } from '@/compositions'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
 </script>
