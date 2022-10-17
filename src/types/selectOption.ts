6da6d3df2d0a0d6137b005537be23ff9b7dbd2cc
Craig Harshbarger <pleek91@gmail.com>
2022-10-17 13:16:03 -0500

    Use `includes` rather than `startsWith`

--- src/types/selectOption.ts
+++ src/types/selectOption.ts
@@ -10,10 +10,10 @@ export function isSelectOption(input: string | number | boolean | SelectOption):
   return typeof input === 'object'
 }
 
-export function optionStartsWith(option: SelectOption, target: string | null): boolean {
+export function optionIncludes(option: SelectOption, target: string | null): boolean {
   if (typeof target !== 'string') {
     return true
   }
 
-  return option.label.toLowerCase().startsWith(target.toLowerCase())
+  return option.label.toLowerCase().includes(target.toLowerCase())
 }
