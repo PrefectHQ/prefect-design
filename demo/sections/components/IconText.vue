996a30b2c2cbcc4960b189add2163024e22c46ef
Josh <josh.furlin@gmail.com>
2022-10-25 10:16:32 -0500

    adjusts icon/text gap, fades icon if inside link

--- demo/sections/components/IconText.vue
+++ demo/sections/components/IconText.vue
@@ -1,16 +1,23 @@
 <template>
-  <ComponentPage title="Icon Text" :demos="[{ title: 'Icon Text' }]">
+  <ComponentPage title="Icon Text" :demos="[{ title: 'Icon Text' }, { title: 'Icon Text Inside Link' }]">
     <template #description>
       This is where we add a short description of <p-code>p-icon-text</p-code>. Describe the components intent, not hyper specific documentation that belongs on vitepress page.
     </template>
 
     <template #icon-text>
-      <p-icon-text v-for="icon in icons" :key="icon" :icon="icon" :text="icon" class="mt-2" />
+      <PIconText v-for="icon in icons" :key="icon" :icon="icon" :text="icon" />
+    </template>
+
+    <template #icon-text-inside-link>
+      <PLink>
+        <PIconText icon="LocationMarkerIcon" text="happy-gazelle" />
+      </PLink>
     </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
+  import { PIconText, PLink } from '@/components'
   import { Icon } from '@/types/icon'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
 
