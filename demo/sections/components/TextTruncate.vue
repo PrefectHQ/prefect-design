1782091d209fa390f245e4c3dff9b289476faeb0
Nicola Reyes <39339887+nicolascribbles@users.noreply.github.com>
2022-11-10 15:36:42 -0700

    Update TextTruncate.vue

--- demo/sections/components/TextTruncate.vue
+++ demo/sections/components/TextTruncate.vue
@@ -4,15 +4,14 @@
       This is where we add a short description of <p-code>p-text-truncate</p-code>. Describe the components intent, not hyper specific documentation that belongs on vitepress page.
     </template>
 
-    <template #text-area>
-      <p-text-truncate :text="exampleText" />
+    <template #multiline>
+      <p-text-truncate text="exampleText" />
     </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
 
-  const exampleText = ref('Cupcake ipsum dolor sit amet. Chocolate pastry icing danish sweet roll marshmallow. Candy sweet roll brownie powder muffin liquorice pie candy canes I love. Jelly beans gingerbread lollipop lemon drops dragée sugar plum tart.')
+  const exampleText = 'Cupcake ipsum dolor sit amet. Chocolate pastry icing danish sweet roll marshmallow. Candy sweet roll brownie powder muffin liquorice pie candy canes I love. Jelly beans gingerbread lollipop lemon drops dragée sugar plum tart.'
 </script>
