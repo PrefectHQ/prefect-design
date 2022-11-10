3c5e2291224b35a2e415230ec9758e82fc32f5f8
Nicola Reyes <39339887+nicolascribbles@users.noreply.github.com>
2022-11-10 14:48:19 -0700

    Adding to the components the main infrastructure

---
+++ demo/sections/components/TextTruncate.vue
@@ -0,0 +1,18 @@
+<template>
+  <ComponentPage title="Text Truncate" :demos="[{ title: 'Text Truncate' }]">
+    <template #description>
+      This is where we add a short description of <p-code>p-text-truncate</p-code>. Describe the components intent, not hyper specific documentation that belongs on vitepress page.
+    </template>
+
+    <template #text-area>
+      <p-text-truncate :text="exampleText" />
+    </template>
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+
+  const exampleText = ref('')
+</script>
---
+++ src/components/TextTruncate/PTextTruncate.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="p-text-truncate">
+    <span>
+      {{ text }}
+    </span>
+    <span>
+      {{ actionText }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed, ref, nextTick, withDefaults } from 'vue'
+  const props = withDefaults(defineProps<{
+    characterCount?: number,
+    text: string,
+    actionText?: string,
+  }>(), {
+    text: '',
+    characterCount: 70,
+    actionText: 'See more',
+  })
+
+  const characterCount = computed(() => props.characterCount)
+  const text = computed(() => props.text)
+  const expanded = ref(false)
+</script>
+
+<style>
+.p-text-truncate {
+  @apply inline-flex;
+}
+</style>
