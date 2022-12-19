589b3c0766168374faa80e8e3b895833a0b4de8f
Evan Sutherland <stackoverfloweth@gmail.com>
2022-12-19 08:21:23 -0600

    simplified validation state model

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,10 +1,5 @@
-/* eslint-disable line-comment-position */
-
 export type State = {
-  touched: boolean, // if the field has been blurred (via handleBlur)
-  pending: boolean, // if validation is in progress
-  dirty: boolean, // if the field has been manipulated (via handleChange)
-  valid: boolean, // if the field doesn't have any errors
-  validated: boolean, // if the field has been validated
-  initialValue?: unknown, // the field's initial value
+  pending: boolean,
+  valid: boolean,
+  validated: boolean,
 }
