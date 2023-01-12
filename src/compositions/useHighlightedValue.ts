6c3c34d2c96e4e7301b288dfe395d35916f0e47b
Evan Sutherland <stackoverfloweth@gmail.com>
2023-01-12 13:24:03 -0600

    code review suggestion

--- src/compositions/useHighlightedValue.ts
+++ src/compositions/useHighlightedValue.ts
@@ -13,7 +13,7 @@ export type UseHighlightedValue = {
 }
 export function useHighlightedValue(selectOptions: MaybeRef<SelectOptionNormalized[]>): UseHighlightedValue {
   const selectOptionsRef = ref(selectOptions)
-  const highlightableValues = computed(() => selectOptionsRef.value
+  const valuesThatCanBeHighlighted = computed(() => selectOptionsRef.value
     .filter(option => !option.disabled)
     .map(({ value }) => value))
 
@@ -24,27 +24,27 @@ export function useHighlightedValue(selectOptions: MaybeRef<SelectOptionNormaliz
   }
 
   function setNextHighlightedValue(): void {
-    const currentIndex = highlightableValues.value.indexOf(highlightedValue.value as SelectModelValue)
-    const newIndex = Math.min(currentIndex + 1, highlightableValues.value.length - 1)
-    highlightedValue.value = highlightableValues.value[newIndex]
+    const currentIndex = valuesThatCanBeHighlighted.value.indexOf(highlightedValue.value as SelectModelValue)
+    const newIndex = Math.min(currentIndex + 1, valuesThatCanBeHighlighted.value.length - 1)
+    highlightedValue.value = valuesThatCanBeHighlighted.value[newIndex]
   }
 
   function setPreviousHighlightedValue(): void {
     if (highlightedValue.value === unselected) {
-      highlightedValue.value = highlightableValues.value[highlightableValues.value.length - 1]
+      highlightedValue.value = valuesThatCanBeHighlighted.value[valuesThatCanBeHighlighted.value.length - 1]
       return
     }
 
-    const currentIndex = highlightableValues.value.indexOf(highlightedValue.value as SelectModelValue)
+    const currentIndex = valuesThatCanBeHighlighted.value.indexOf(highlightedValue.value as SelectModelValue)
     const newIndex = Math.max(currentIndex - 1, 0)
-    highlightedValue.value = highlightableValues.value[newIndex]
+    highlightedValue.value = valuesThatCanBeHighlighted.value[newIndex]
   }
 
   function setHighlightedValueUnselected(): void {
     highlightedValue.value = unselected
   }
 
-  watch(highlightableValues, (options) => {
+  watch(valuesThatCanBeHighlighted, (options) => {
     if (highlightedValue.value === unselected) {
       return
     }
