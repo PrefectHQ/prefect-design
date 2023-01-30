cd1bcf146838f8035eb991710dd50e5a55aef9fa
Evan Sutherland <stackoverfloweth@gmail.com>
2023-01-30 13:45:44 -0600

    remove p-code-file component

--- demo/sections/components/index.ts
+++ demo/sections/components/index.ts
@@ -9,7 +9,6 @@ export const components: Section = {
   checkbox: () => import('./Checkbox.vue'),
   checkboxGroup: () => import('./CheckboxGroup.vue'),
   code: () => import('./Code.vue'),
-  codeFile: () => import('./CodeFile.vue'),
   combobox: () => import('./Combobox.vue'),
   dateInput: () => import('./DateInput.vue'),
   dateRangeInput: () => import('./DateRangeInput.vue'),
--- src/components/CodeFile/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue'
-import PCodeFile from '@/components/CodeFile/PCodeFile.vue'
-
-const install = (app: App): void => {
-  app.component('PCodeFile', PCodeFile)
-}
-
-export { PCodeFile, install }
