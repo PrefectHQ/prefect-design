918ee641baef11c171c5605e2a057cf9dff5098c
Evan Sutherland <stackoverfloweth@gmail.com>
2023-01-31 13:31:08 -0600

    broke out p-tab-nav and p-tab-select from p-tabs

---
+++ src/components/Tabs/PTabNav.vue
@@ -0,0 +1,73 @@
+<template>
+  <ul class="p-tab-nav" role="tablist" aria-label="Tab">
+    <template v-for="(tab, index) in tabs" :key="tab.label">
+      <PTab
+        :id="kebabCase(tab.label)"
+        :active="selected === tab.label"
+        :disabled="tab.disabled"
+        :area-controls="`${kebabCase(tab.label)}-content`"
+        @click="selected = tab.label"
+        @keydown.enter.space.prevent="handleSpaceDown(tab)"
+      >
+        <slot
+          :name="`${kebabCase(tab.label)}-heading`"
+          v-bind="{ tab: tab.label, index }"
+        >
+          {{ tab.label }}
+        </slot>
+      </PTab>
+    </template>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import PTab from '@/components/Tab/PTab.vue'
+  import { Tab, normalizeTab } from '@/types'
+  import { kebabCase } from '@/utilities/strings'
+
+  const props = defineProps<{
+    tabs: (string | Tab)[],
+    selected?: string,
+  }>()
+
+  const emits = defineEmits<{
+    (event: 'update:selected', value: string): void,
+  }>()
+
+  const tabs = computed(() => props.tabs.map(normalizeTab))
+
+  const selected = computed({
+    get() {
+      if (props.selected) {
+        return props.selected
+      }
+
+      const [first] = tabs.value
+
+      return first.label
+    },
+    set(value) {
+      emits('update:selected', value)
+    },
+  })
+
+  function handleSpaceDown(tab: Tab): void {
+    if (tab.disabled) {
+      return
+    }
+
+    selected.value = tab.label
+  }
+</script>
+
+<style>
+.p-tab-nav { @apply
+  border-b
+  border-background-400
+  dark:border-background-600
+  flex
+  items-center
+  -mb-px
+}
+</style>
--- src/types/tabs.ts
+++ src/types/tabs.ts
@@ -7,6 +7,12 @@ export function isTab(value: unknown): value is Tab {
   return typeof value === 'object' && value !== null && 'label' in value
 }
 
-export function areTabs(values: unknown[]): values is Tab[] {
-  return values.every(isTab)
+export function normalizeTab(value: string | Tab): Tab {
+  if (isTab(value)) {
+    return value
+  }
+
+  return {
+    label: value,
+  }
 }
