bb36ae5286606a10e1f608f9434fdd7d459743b5
nicholas <znicholasbrown@gmail.com>
2023-02-05 15:59:16 -0500

    Remove sanitization from the worker - it can't run in a node environment without JSDOM and maybe not at all

--- src/components/MarkdownRenderer/worker.ts
+++ src/components/MarkdownRenderer/worker.ts
@@ -1,46 +1,20 @@
-import { default as dompurify } from 'dompurify'
 import { marked } from 'marked'
-import { ParseMessagePayload, SanitizeMessagePayload, MarkdownMessagePayload } from '@/types/markdownRenderer'
-
-const forbiddenAttrs = ['style']
-const useProfiles = { svg: true, html: true }
+import { ParseMessagePayload, MarkdownMessagePayload } from '@/types/markdownRenderer'
 
 const markedOptions: marked.MarkedOptions = {
   gfm: true,
   breaks: true,
-  sanitizer: (html: string): string => {
-    return dompurify.sanitize(html,
-      {
-        FORBID_ATTR: forbiddenAttrs,
-        USE_PROFILES: useProfiles,
-      })
-  },
 }
 
 const parse = (text: string): marked.TokensList => {
   return marked.lexer(text, markedOptions)
 }
 
-const sanitize = (text: string): string => {
-  return dompurify.sanitize(text,
-    {
-      FORBID_ATTR: forbiddenAttrs,
-      USE_PROFILES: useProfiles,
-    })
-}
-
 const handleMessage = (message: MessageEvent<MarkdownMessagePayload>): void => {
-  const { text, type } = message.data
-
-  if (type == 'parse') {
-    const tokens = parse(text)
-    const response: ParseMessagePayload = { type, tokens }
-    self.postMessage(response)
-  } else {
-    const sanitized = sanitize(text)
-    const response: SanitizeMessagePayload = { type, sanitized }
-    self.postMessage(response)
-  }
+  const { text } = message.data
+  const tokens = parse(text)
+  const response: ParseMessagePayload = { tokens }
+  self.postMessage(response)
 }
 
 self.onmessage = handleMessage
