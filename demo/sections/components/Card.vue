8a9daf205cf7d977a61dbef490c1c926ce5e0f5d
Evan Sutherland <stackoverfloweth@gmail.com>
2023-02-21 09:06:00 -0600

    applying some styling to p-code-highlight when inside of p-card

--- demo/sections/components/Card.vue
+++ demo/sections/components/Card.vue
@@ -7,13 +7,36 @@
     </template>
 
     <template #card>
-      <p-card>
-        <h1>This is a Card component</h1>
+      <p-card class="card-demo">
+        <span class="card-demo-heading">Lorem with code</span>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem neque dolor obcaecati consequuntur repellendus. Accusantium ratione obcaecati voluptatum dolorum numquam accusamus praesentium asperiores iste laboriosam, consequatur necessitatibus? Placeat, tempore facere!
+        <PCodeHighlight :text="json" lang="json" />
       </p-card>
     </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
+  import PCodeHighlight from '@/components/CodeHighlight/PCodeHighlight.vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
+  const json = `
+{
+  "foo": "bar",
+  "yes": false,
+  "single": ["is", "it", "really", "?"],
+  "multiple": "no"
+}
+`.trim()
 </script>
+
+<style>
+.card-demo { @apply
+  flex
+  flex-col
+  gap-4
+}
+
+.card-demo-heading { @apply
+  text-2xl
+}
+</style>
--- src/components/Card/PCard.vue
+++ src/components/Card/PCard.vue
@@ -15,4 +15,15 @@
   rounded-lg
   shadow-sm
 }
+
+.p-card .p-code-highlight { @apply
+  px-4
+  py-2
+  dark:bg-slate-600
+  bg-slate-200
+}
+
+.p-card .p-code-highlight__code-wrapper {
+  color: unset;
+}
 </style>
