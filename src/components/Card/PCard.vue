2708cef72cb949afe1df6f58419c4413dbdf10c4
nicholas <znicholasbrown@gmail.com>
2023-03-23 19:52:02 -0400

    Remove box-shadow transition

--- src/components/Card/PCard.vue
+++ src/components/Card/PCard.vue
@@ -27,6 +27,14 @@
   rounded-lg
   shadow-sm
   transition-all;
+
+  /*
+    Explicitly defining these after transition-all
+      to override the transition-property on the box-shadow
+      because transitioning shadows isn't super performant
+      and it doesn't add much value.
+  */
+  transition-property: border border-color border-radius background-color color padding;
 }
 
 .p-card .p-card--flat { @apply
