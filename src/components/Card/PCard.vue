2733a1dd3347d207d303243e026c330097e2363a
nicholas <znicholasbrown@gmail.com>
2023-03-24 12:31:15 -0400

    Address PR feedback

--- src/components/Card/PCard.vue
+++ src/components/Card/PCard.vue
@@ -8,7 +8,7 @@
   import { computed } from 'vue'
 
   const props = defineProps<{
-    flat: boolean,
+    flat?: boolean,
   }>()
 
   const classes = computed(() => ({
@@ -28,16 +28,10 @@
   shadow-sm
   transition-all;
 
-  /*
-    Explicitly defining these after transition-all
-      to override the transition-property on the box-shadow
-      because transitioning shadows isn't super performant
-      and it doesn't add much value.
-  */
-  transition-property: border border-color border-radius background-color color padding;
+  transition-property: border border-color background-color;
 }
 
-.p-card .p-card--flat { @apply
+.p-card--flat { @apply
   border-transparent
   shadow-none
   bg-transparent
