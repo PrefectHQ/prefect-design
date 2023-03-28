795c95be8ec6efb95ab82750b817f3f93c77dac8
nicholas <znicholasbrown@gmail.com>
2023-03-27 21:58:35 -0400

    Add virtual scroller

--- src/components/OrderedList/POrderedList.vue
+++ src/components/OrderedList/POrderedList.vue
@@ -1,54 +1,69 @@
 <template>
   <ol class="p-ordered-list">
-    <template v-for="(item, index) in items" :key="getItemId(item, index)">
-      <slot v-bind="{ item, index }">
-        <slot :name="getListItemSlotName(item, index)" v-bind="{ item, index }">
-          <POrderedListItem v-bind="{ item }" class="p-ordered-list__item">
-            <template #left>
-              <slot name="left" v-bind="{ item, index }">
-                <slot :name="getListItemLeftSlotName(item, index)" v-bind="{ item, index }" />
-              </slot>
-            </template>
-
-            <template #node>
-              <slot name="node" v-bind="{ item, index }">
-                <slot :name="getListItemNodeSlotName(item, index)" v-bind="{ item, index }">
-                  <POrderedListNode>
-                    <slot :name="getListItemNodeContentSlotName(item, index)" v-bind="{ item, index }">
-                      <slot name="node-content" v-bind="{ item, index }">
-                        <p-icon v-if="item.icon" :icon="item.icon" solid />
-                      </slot>
-                    </slot>
-                  </POrderedListNode>
+    <PVirtualScroller
+      :items="items"
+      :item-key="itemKey"
+      :item-estimate-height="itemEstimateHeight"
+      :chunk-size="chunkSize"
+      @bottom="emit('bottom')"
+    >
+      <template #default="{ item, index }: { item: OrderedListItem, index:number }">
+        <slot v-bind="{ item, index }">
+          <slot :name="getListItemSlotName(item, index)" v-bind="{ item, index }">
+            <POrderedListItem v-bind="{ item }" class="p-ordered-list__item">
+              <template #left>
+                <slot name="left" v-bind="{ item, index }">
+                  <slot :name="getListItemLeftSlotName(item, index)" v-bind="{ item, index }" />
                 </slot>
-              </slot>
-            </template>
+              </template>
 
-            <template #right>
-              <slot name="right" v-bind="{ item, index }">
-                <slot :name="getListItemRightSlotName(item, index)" v-bind="{ item, index }" />
-              </slot>
-            </template>
-          </POrderedListItem>
+              <template #node>
+                <slot name="node" v-bind="{ item, index }">
+                  <slot :name="getListItemNodeSlotName(item, index)" v-bind="{ item, index }">
+                    <POrderedListNode>
+                      <slot :name="getListItemNodeContentSlotName(item, index)" v-bind="{ item, index }">
+                        <slot name="node-content" v-bind="{ item, index }">
+                          <p-icon v-if="item.icon" :icon="item.icon" solid />
+                        </slot>
+                      </slot>
+                    </POrderedListNode>
+                  </slot>
+                </slot>
+              </template>
+
+              <template #right>
+                <slot name="right" v-bind="{ item, index }">
+                  <slot :name="getListItemRightSlotName(item, index)" v-bind="{ item, index }" />
+                </slot>
+              </template>
+            </POrderedListItem>
+          </slot>
         </slot>
-      </slot>
-    </template>
+      </template>
+    </PVirtualScroller>
   </ol>
 </template>
 
 <script lang="ts" setup>
   import POrderedListItem from '@/components/OrderedList/POrderedListItem.vue'
   import POrderedListNode from '@/components/OrderedList/POrderedListNode.vue'
+  import PVirtualScroller from '@/components/VirtualScroller/PVirtualScroller.vue'
   import { OrderedListItem } from '@/types/orderedList'
   import { kebabCase } from '@/utilities/strings'
 
   const props = defineProps<{
     items: OrderedListItem[],
-    itemIdKey?: string,
+    itemKey?: string,
+    itemEstimateHeight?: number,
+    chunkSize?: number,
+  }>()
+
+  const emit = defineEmits<{
+    (event: 'bottom'): void,
   }>()
 
   function getItemId(item: OrderedListItem, index: number): string | number {
-    return props.itemIdKey ? kebabCase(`${item[props.itemIdKey]}`) : index
+    return props.itemKey ? kebabCase(`${item[props.itemKey]}`) : index
   }
 
   function getListItemSlotName(item: OrderedListItem, index: number): string {
