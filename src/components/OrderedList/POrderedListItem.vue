749679d456e10179fdc3397c3557994be263b169
nicholas <znicholasbrown@gmail.com>
2023-03-28 10:27:03 -0400

    Remove unused class

--- src/components/OrderedList/POrderedListItem.vue
+++ src/components/OrderedList/POrderedListItem.vue
@@ -79,16 +79,4 @@
 
   content: '';
 }
-
-.p-ordered-list-item__node { @apply
-  rounded-full
-  bg-background
-  border
-  dark:border-foreground-100
-  text-foreground-400
-  p-2
-  text-center
-  shadow
-  z-[1]
-}
 </style>
