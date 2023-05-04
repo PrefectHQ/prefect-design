a51500f5a06962a239e7ef1ddc3af6970f69897b
nicholas <znicholasbrown@gmail.com>
2023-05-04 15:09:38 -0400

    Move sidebar layout demo

--- demo/sections/layouts/index.ts
+++ demo/sections/layouts/index.ts
@@ -4,7 +4,6 @@ export const layouts: Section = {
   layoutDefault: () => import('./LayoutDefault.vue'),
   layoutFull: () => import('./LayoutFull.vue'),
   layoutResizable: () => import('./LayoutResizable.vue'),
-  layoutSidebar: () => import('./LayoutSidebar.vue'),
   layoutTable: () => import('./LayoutTable.vue'),
   layoutWell: () => import('./LayoutWell.vue'),
 }
