354ef157703f661943ba70693023a9b29a97014f
nicholas <znicholasbrown@gmail.com>
2023-05-05 15:34:33 -0400

    Add basic drawer demo

---
+++ demo/sections/components/Drawer.vue
@@ -0,0 +1,55 @@
+<template>
+  <ComponentPage title="Drawer" :demos="demos">
+    <template #description>
+      <p-select v-model="position" :options="positions" />
+    </template>
+
+    <template #basic>
+      <p-button @click="toggle('basic')">
+        Open
+      </p-button>
+
+      <p-drawer v-model="drawers.basic" :position="position">
+        Basic drawer
+      </p-drawer>
+    </template>
+
+    <template #advanced>
+      <p-button @click="toggle('advanced')">
+        Open
+      </p-button>
+
+      <p-drawer v-model="drawers.advanced" :position="position">
+        <p-navigation-bar>
+          Advanced drawer
+        </p-navigation-bar>
+      </p-drawer>
+    </template>
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import { reactive, ref } from 'vue'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+
+  const demos = [
+    {
+      title: 'Basic',
+    },
+    {
+      title: 'Advanced',
+    },
+  ]
+
+  const position = ref('left')
+  const positions = ['left', 'right', 'top', 'bottom']
+
+  const drawers = reactive({
+    basic: false,
+    advanced: false,
+  })
+
+  const toggle = (name: keyof typeof drawers): void => {
+    drawers[name] = !drawers[name]
+  }
+</script>
