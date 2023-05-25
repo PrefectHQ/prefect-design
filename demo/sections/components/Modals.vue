0cf377e2c2d911a8a99eabca47aff4e99661f386
Craig Harshbarger <pleek91@gmail.com>
2023-05-25 14:05:18 -0500

    Scroll the modal body rather than the whole modal

--- demo/sections/components/Modals.vue
+++ demo/sections/components/Modals.vue
@@ -1,5 +1,5 @@
 <template>
-  <ComponentPage title="Modals" :demos="[{ title: 'Modals' }, { title: 'Auto-Close' }]">
+  <ComponentPage title="Modals" :demos="demos">
     <template #description>
       This is where we add a short description of <p-code inline>
         p-modals
@@ -18,6 +18,54 @@
 
         <template #actions>
           <p-button @click="showModal = false">
+            Submit
+          </p-button>
+        </template>
+      </p-modal>
+    </template>
+
+    <template #long-content>
+      <p-button @click="showLongContentModal = true">
+        Open Modal
+      </p-button>
+
+      <p-modal v-model:showModal="showLongContentModal" title="Modal Title" icon="CakeIcon">
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+        <p class="text-sm text-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in aliquam erat. Proin elit dui, tristique non consequat at, gravida ac lectus
+        </p>
+
+        <template #actions>
+          <p-button @click="showLongContentModal = false">
             Submit
           </p-button>
         </template>
@@ -48,6 +96,13 @@
   import { ref } from 'vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
 
+  const demos = [
+    { title: 'Modals' },
+    { title: 'Auto Close' },
+    { title: 'Long Content' },
+  ]
+
   const showModal = ref(false)
+  const showLongContentModal = ref(false)
   const showAutoCloseModal = ref(false)
 </script>
