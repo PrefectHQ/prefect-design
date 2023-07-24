929e8d6d875da693d512b69ca5e91cb18ad33265
nicholas <znicholasbrown@gmail.com>
2023-07-24 17:58:57 -0400

    Add error boundary component

---
+++ src/components/ErrorBoundary/PErrorBoundary.vue
@@ -0,0 +1,15 @@
+<template>
+  <slot v-if="!error" />
+  <slot v-else name="fallback" />
+</template>
+
+<script lang="ts" setup>
+  import { onErrorCaptured, ref } from 'vue'
+
+  const error = ref<string | null>(null)
+
+  onErrorCaptured((err) => {
+    error.value = err.message
+    return false
+  })
+</script>
