1f0b7bc0afc99afa1503836c00f71d0dcc9f0b6a
Brandon Reid <brandonreidak@gmail.com>
2023-08-11 11:57:34 -0300

    add design token docs

--- demo/sections/index.ts
+++ demo/sections/index.ts
@@ -4,6 +4,7 @@ import { layouts } from '@/demo/sections/layouts'
 
 export const sections: Section = {
   components,
+  designTokens: () => import('./DesignTokens.vue'),
   icons: () => import('./Icons.vue'),
   layouts,
 }
