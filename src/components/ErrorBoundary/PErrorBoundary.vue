779081e144a4c3b5b44174810b45c17a7f1fd20a
nicholas <znicholasbrown@gmail.com>
2023-09-13 12:33:31 -0400

    Add emitter, bind error to slot

--- src/components/ErrorBoundary/PErrorBoundary.vue
+++ src/components/ErrorBoundary/PErrorBoundary.vue
@@ -1,15 +1,20 @@
 <template>
   <slot v-if="!error" />
-  <slot v-else name="fallback" />
+  <slot v-else name="fallback" v-bind="{ error }" />
 </template>
 
 <script lang="ts" setup>
   import { onErrorCaptured, ref } from 'vue'
 
-  const error = ref<string | null>(null)
+  const emit = defineEmits<{
+    (event: 'error', value: Error): void,
+  }>()
+
+  const error = ref<Error>(null)
 
   onErrorCaptured((err) => {
-    error.value = err.message
+    error.value = err
+    emit('error', error.value)
     return false
   })
 </script>
