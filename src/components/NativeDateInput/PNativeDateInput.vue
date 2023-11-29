3fb474c9e35e4df03f34fdca761c2593083b07c1
Craig Harshbarger <pleek91@gmail.com>
2023-11-29 10:50:22 -0600

    Clean up the native date input component

--- src/components/NativeDateInput/PNativeDateInput.vue
+++ src/components/NativeDateInput/PNativeDateInput.vue
@@ -3,84 +3,47 @@
     <template v-for="(index, name) in $slots" #[name]="data">
       <slot :name="name" v-bind="data" />
     </template>
+
     <template #control="{ attrs }">
-      <template v-if="dateMode">
-        <input
-          ref="inputElement"
-          v-model="stringValue"
-          type="date"
-          class="p-native-date-input__control"
-          :min="stringMin"
-          :max="stringMax"
-          v-bind="attrs"
-        >
-        <input
-          ref="inputElement"
-          v-model="stringValue"
-          type="date"
-          class="p-native-date-input__control p-native-date-input__control--placeholder"
-          :min="stringMin"
-          :max="stringMax"
-          v-bind="attrs"
-        >
-      </template>
-
-      <template v-else>
-        <div class="p-native-date-input__time-pickers">
-          <p-native-select v-model="selectedHours" class="p-native-date-input__picker" :options="hourOptions" />
-          <p-native-select v-model="selectedMinutes" class="p-native-date-input__picker" :options="minuteOptions" />
-          <p-native-select v-model="selectedMeridiem" class="p-native-date-input__picker" :options="meridiemOptions" />
-        </div>
-      </template>
-
-      <template v-if="showTime">
-        <p-button
-          class="p-native-date-input__mode-toggle"
-          small
-          flat
-          icon="ClockIcon"
-          @click="dateMode = !dateMode"
-        />
-      </template>
+      <input
+        ref="inputElement"
+        v-model="stringValue"
+        type="date"
+        class="p-native-date-input__control"
+        :min="stringMin"
+        :max="stringMax"
+        v-bind="attrs"
+      >
     </template>
 
     <template #append>
-      <div class="p-native-date-input__icon">
-        <PIcon icon="CalendarIcon" />
-      </div>
+      <PButton icon="CalendarIcon" small flat class="p-native-date-input__picker" @click="showPicker" />
     </template>
   </PBaseInput>
 </template>
 
 <script lang="ts" setup>
-  import { format, parseISO, startOfMinute } from 'date-fns'
-  import { computed, ref } from 'vue'
+  import { format, parseISO } from 'date-fns'
+  import { computed, readonly, ref } from 'vue'
   import PBaseInput from '@/components/BaseInput/PBaseInput.vue'
-  import PIcon from '@/components/Icon/PIcon.vue'
-  import { useTimeParts } from '@/compositions/useTimeParts'
-  import { keepDateInRange } from '@/utilities/dates'
 
   const props = defineProps<{
     modelValue: Date | null | undefined,
     min?: Date | null,
     max?: Date | null,
-    showTime?: boolean,
   }>()
 
   const emit = defineEmits<{
     (event: 'update:modelValue', value: Date | null): void,
   }>()
 
-  const dateMode = ref(true)
-  const internalValue = computed(() => props.modelValue ?? null)
   const inputElement = ref<HTMLInputElement>()
-  const el = computed(() => inputElement.value)
 
-  defineExpose({ inputElement, el })
+  defineExpose({ el: readonly(inputElement) })
 
   const stringValue = computed({
     get() {
-      return internalValue.value ? format(internalValue.value, 'yyyy-MM-dd') : null
+      return props.modelValue ? format(props.modelValue, 'yyyy-MM-dd') : null
     },
     set(value) {
       emit('update:modelValue', value ? parseISO(value) : null)
@@ -90,90 +53,31 @@
   const stringMin = computed(() => props.min ? format(props.min, 'yyyy-MM-dd') : undefined)
   const stringMax = computed(() => props.max ? format(props.max, 'yyyy-MM-dd') : undefined)
 
-  const selectedDate = computed({
-    get() {
-      return props.modelValue ?? startOfMinute(new Date())
-    },
-    set(value) {
-      const withoutSeconds = startOfMinute(value)
-
-      emit('update:modelValue', keepDateInRange(withoutSeconds, range.value))
-    },
-  })
-
-  const range = computed(() => ({ min: props.min, max: props.max }))
-
-  const {
-    selectedHours,
-    selectedMinutes,
-    selectedMeridiem,
-    hourOptions,
-    minuteOptions,
-    meridiemOptions,
-  } = useTimeParts(selectedDate, range)
+  function showPicker(): void {
+    inputElement.value?.showPicker()
+  }
 </script>
 
 <style>
-.p-native-date-input { @apply
-  relative
-  appearance-none
-  bg-none
-}
-
-.p-native-date-input__icon { @apply
-  pr-3
-  flex
-  items-center
-  z-10
-  pointer-events-none
-}
-
 .p-native-date-input__control { @apply
   bg-transparent
-  absolute
-  top-0
-  bottom-0
-  left-0
-  right-0
+  block
   w-full
   rounded-default
   border-0
   focus:ring-0
 }
 
-.p-native-date-input__control:disabled { @apply
-  cursor-not-allowed
-}
-
-.p-native-date-input__control--placeholder { @apply
-  relative
-  invisible
-  w-full
-}
-
-.p-native-date-input .p-native-date-input__control::-webkit-calendar-picker-indicator { @apply
-  cursor-pointer
-  bg-none
-}
-
-.p-native-date-input .p-native-date-input__control--placeholder::-webkit-calendar-picker-indicator {
+.p-native-date-input__control::-webkit-calendar-picker-indicator {
   display: none;
   -webkit-appearance: none;
 }
 
-.p-native-date-input__mode-toggle { @apply
-  mr-2
-}
-
-.p-native-date-input__time-pickers { @apply
-  flex
-  flex-grow
-  justify-around
+.p-native-date-input__control:disabled { @apply
+  cursor-not-allowed
 }
 
 .p-native-date-input__picker { @apply
-  w-min
-  border-0
-  !ring-0
+  mr-2
 }
 </style>
