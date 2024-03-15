75d79eba1741cfe08938aa2c4533d53d55fa4dee
nicholas <znicholasbrown@gmail.com>
2024-03-15 16:46:44 -0400

    Use a more generic type for position around point

--- src/utilities/positionRelative.ts
+++ src/utilities/positionRelative.ts
@@ -1,8 +1,7 @@
-import { MousePosition } from '@prefecthq/vue-compositions'
 import { MaybeRefOrGetter, toValue } from 'vue'
 import { PositionMethod, Position } from '@/types/position'
 
-export const positionAroundPoint = function(position: MaybeRefOrGetter<MousePosition>, method: PositionMethod): PositionMethod {
+export const positionAroundPoint = function(position: MaybeRefOrGetter<{ x: number, y: number }>, method: PositionMethod): PositionMethod {
   return function(target: DOMRect, content: DOMRect, container: DOMRect): Position {
     const { x, y } = toValue(position)
     const targetPosition = new DOMRect(x, y, 0, 0)
