c2343c6b91f6adfdc8f0b976b610d3c8cedb4b0e
Craig Harshbarger <pleek91@gmail.com>
2024-04-02 14:41:20 -0500

    Remove defineProps imports

--- src/components/SelectOptionGroup/PSelectOptionGroup.vue
+++ src/components/SelectOptionGroup/PSelectOptionGroup.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script lang="ts" setup>
-  import { defineProps, computed } from 'vue'
+  import { computed } from 'vue'
   import { SelectOptionGroupNormalized } from '@/types'
 
   const props = defineProps<{
