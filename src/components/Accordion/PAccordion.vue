90cff415df4437bba4e749f042f52631b5a0a4cc
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-15 20:27:10 -0400

    Update PAccordion.vue

--- src/components/Accordion/PAccordion.vue
+++ src/components/Accordion/PAccordion.vue
@@ -1,144 +1,57 @@
 <template>
-  <div class="p-accordion">
+  <AccordionRoot type="single" :collapsible="true">
     <template v-for="section in sections" :key="section">
-      <div class="p-accordion__section">
-        <slot
-          :id="getHeadingId(section)"
-          name="header"
-          :section="section"
-          :selected="isSelected(section)"
-          :content="getContentId(section)"
-          :toggle="() => toggleSection(section)"
-        >
-          <button
-            :id="getHeadingId(section)"
-            type="button"
-            :aria-expanded="isSelected(section)"
-            :aria-controls="getContentId(section)"
-            class="p-accordion__header"
-            @click="toggleSection(section)"
-          >
-            <span class="p-accordion__heading">
-              <slot :name="getHeadingId(section)">
-                <slot name="heading" :section="section" :selected="isSelected(section)">
-                  {{ section }}
-                </slot>
-              </slot>
-            </span>
-            <slot name="icon" :section="section">
-              <p-icon size="small" icon="ChevronDownIcon" class="p-accordion__icon" :class="getIconClass(section)" />
-            </slot>
-          </button>
-        </slot>
-
-        <PAutoHeightTransition>
-          <template v-if="isSelected(section)">
-            <div
-              :id="getContentId(section)"
-              class="p-accordion__content"
-              role="region"
-              :aria-labelledby="getHeadingId(section)"
-            >
-              <!-- this is needed because PAutoHeightTransition does not animate an elements padding correctly -->
-              <div class="p-accordion__content-padding">
-                <slot :name="getContentId(section)">
-                  <slot name="content" :section="section" :selected="isSelected(section)" />
-                </slot>
-              </div>
-            </div>
-          </template>
-        </PAutoHeightTransition>
-      </div>
+      <AccordionItem :value="section" class="p-accordion__item">
+        <AccordionHeader class="p-accordion__header">
+          <AccordionTrigger class="group p-accordion__trigger">
+            <span>{{ section }}</span>
+            <p-icon size="small" icon="ChevronDownIcon" class="p-accordion__icon" />
+          </AccordionTrigger>
+        </AccordionHeader>
+        <AccordionContent class="p-accordion__content">
+          <slot name="content" :section="section" />
+        </AccordionContent>
+      </AccordionItem>
     </template>
-  </div>
+  </AccordionRoot>
 </template>
 
 <script lang="ts" setup generic="const T extends string">
-  import { Ref, computed, ref } from 'vue'
-  import { PAutoHeightTransition } from '@/components/AutoHeightTransition'
-  import { kebabCase } from '@/utilities'
+  import { AccordionContent, AccordionHeader, AccordionItem, AccordionRoot, AccordionTrigger } from 'radix-vue'
+  import { ref } from 'vue'
 
-  const props = defineProps<{
+  defineProps<{
     sections: T[],
-    selected?: T | null,
   }>()
-
-  const emit = defineEmits<{
-    (event: 'update:selected', value: T | null): void,
-  }>()
-
-  const backupSelected: Ref<T | null> = ref(null)
-
-  const selected = computed({
-    get() {
-      if (props.selected) {
-        return props.selected
-      }
-
-      if (backupSelected.value) {
-        return backupSelected.value
-      }
-
-      return null
-    },
-    set(selected: T | null) {
-      backupSelected.value = selected
-
-      emit('update:selected', selected)
-    },
-  })
-
-  function toggleSection(section: T): void {
-    if (isSelected(section)) {
-      selected.value = null
-      return
-    }
-
-    selected.value = section
-  }
-
-  function isSelected(section: T): boolean {
-    return selected.value === section
-  }
-
-  function getIconClass(section: T): string | undefined {
-    if (isSelected(section)) {
-      return 'p-accordion__icon--open'
-    }
-  }
-
-  function getHeadingId(section: T): string {
-    return `${kebabCase(section)}-heading`
-  }
-
-  function getContentId(section: T): string {
-    return `${kebabCase(section)}-content`
-  }
 </script>
 
 <style>
 .p-accordion__header { @apply
-  w-full
-  flex
-  gap-2
-  items-center
-  justify-between
-  py-2
-  text-base
-  text-default
-  border-t
-  border-default
+  text-sm
+  hover:underline
 }
 
-.p-accordion__content-padding { @apply
-  pb-2
+.p-accordion__item { @apply
+  data-[state=open]:pb-4
+  border-b-[.5px]
+}
+
+.p-accordion__trigger { @apply
+  flex
+  justify-between
+  w-full
+  py-4
 }
 
 .p-accordion__icon { @apply
-  transition-transform
+  ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300
+  group-aria-[expanded=true]:rotate-180
+  my-auto
 }
 
-.p-accordion__icon--open { @apply
-  rotate-180
+.p-accordion__content{ @apply
+  overflow-hidden
+  data-[state=open]:animate-accordion-down
+  data-[state=closed]:animate-accordion-up
 }
 </style>
