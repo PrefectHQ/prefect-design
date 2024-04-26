04a86a2e54611947e6ce1319356923b85609441c
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-25 21:42:27 -0400

    rm use of clsx

--- src/utilities/tailwind.ts
+++ src/utilities/tailwind.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { normalizeClass } from 'vue'
 
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs))
+
+export function cn(...inputs: Parameters<typeof normalizeClass>): string {
+  return twMerge(normalizeClass(inputs))
+}
+
+type ClassValue = ClassArray | ClassDictionary | string | number | bigint | null | boolean | undefined
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ClassDictionary = Record<string, any>
+type ClassArray = ClassValue[]
+
+declare module 'vue' {
+  function normalizeClass(...inputs: ClassValue[]): string
 }
