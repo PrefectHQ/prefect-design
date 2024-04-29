5f55ad0d70cdc075150dfbaf723be50b7ae53848
Collin Choy <collin@prefect.io>
2024-04-29 13:41:01 -0400

    Remove max-width.

--- src/components/Combobox/RComboboxTagsInput.vue
+++ src/components/Combobox/RComboboxTagsInput.vue
@@ -1,7 +1,7 @@
 <template>
   <TagsInputRoot
     v-model="modelValue"
-    class="flex gap-2 items-center border p-2 rounded-default w-full max-w-[480px] flex-wrap border-[var(--p-color-input-border)] bg-[var(--p-color-input-bg)]"
+    class="flex gap-2 items-center border p-2 rounded-default w-full flex-wrap border-[var(--p-color-input-border)] bg-[var(--p-color-input-bg)]"
   >
     <TagsInputItem v-for="item in modelValue" :key="item" :value="item" class="text-[var(--p-color-tag-text)] flex shadow-md items-center justify-center gap-2 bg-[var(--p-color-tag-bg)] aria-[current=true]:border-2 border-[var(--p-color-divider)] rounded-default p-1">
       <TagsInputItemText class="text-sm pl-1" />
