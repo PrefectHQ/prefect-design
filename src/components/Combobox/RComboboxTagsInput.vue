170226d7700b686d03b1b1009f6cb8ac11fba0aa
Collin Choy <collin@prefect.io>
2024-04-29 11:54:08 -0400

    style updates.

--- src/components/Combobox/RComboboxTagsInput.vue
+++ src/components/Combobox/RComboboxTagsInput.vue
@@ -1,16 +1,16 @@
 <template>
   <TagsInputRoot
     v-model="modelValue"
-    class="flex gap-2 items-center border px-2 py-1 rounded-lg w-full max-w-[480px] flex-wrap border-[var(--p-color-input-border)] bg-[var(--p-color-input-bg)]"
+    class="flex gap-2 items-center border p-2 rounded-default w-full max-w-[480px] flex-wrap border-[var(--p-color-input-border)] bg-[var(--p-color-input-bg)]"
   >
-    <TagsInputItem v-for="item in modelValue" :key="item" :value="item" class="text-white flex shadow-md items-center justify-center gap-2 bg-[var(--p-color-tag-bg)] aria-[current=true]:border-2 border-[var(--p-color-divider)] rounded p-1">
+    <TagsInputItem v-for="item in modelValue" :key="item" :value="item" class="text-[var(--p-color-tag-text)] flex shadow-md items-center justify-center gap-2 bg-[var(--p-color-tag-bg)] aria-[current=true]:border-2 border-[var(--p-color-divider)] rounded-default p-1">
       <TagsInputItemText class="text-sm pl-1" />
       <TagsInputItemDelete class="p-0.5 rounded bg-transparent hover:bg-[var(--p-color-selectable-hover)]">
         <PIcon icon="XMarkIcon" size="small" />
       </TagsInputItemDelete>
     </TagsInputItem>
 
-    <TagsInputInput placeholder="Fruits..." class="text-sm focus:outline-none flex-1 rounded border-none bg-transparent placeholder:text-subdued px-1" />
+    <TagsInputInput placeholder="Fruits..." class="text-sm focus:outline-none flex-1 rounded-default border-none bg-transparent placeholder:text-subdued px-1" />
   </TagsInputRoot>
 </template>
 
