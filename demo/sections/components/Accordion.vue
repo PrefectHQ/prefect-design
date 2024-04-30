bcb1a6090ac7aab4ffddcf57c93c91c893fe7979
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-30 10:22:50 -0400

    Update Accordion.vue

--- demo/sections/components/Accordion.vue
+++ demo/sections/components/Accordion.vue
@@ -2,15 +2,6 @@
   <ComponentPage title="Accordion" :demos="[{ title: 'Accordion' }, { title: 'Controlled Accordion' }]">
     <template #accordion>
       <PAccordion :sections="['one', 'two', 'three', 'four']">
-        <template #one-heading>
-          This is one using a slot
-        </template>
-        <template #one-content>
-          This is the content for number one
-        </template>
-        <template #two-content>
-          This is the content for number two
-        </template>
         <template #content="{ section }">
           This is the default content for section {{ section }}
         </template>
@@ -19,15 +10,6 @@
     <template #controlled-accordion>
       <p-number-input v-model="number" />
       <PAccordion :sections="['0', '1', '2', '3']" :model-value="(number % 4).toString()" @update:model-value="number = parseInt($event)">
-        <template #1-heading>
-          This is one using a slot
-        </template>
-        <template #2-content>
-          This is the content for number one
-        </template>
-        <template #3-content>
-          This is the content for number two
-        </template>
         <template #content="{ section }">
           This is the default content for section {{ section }}
         </template>
