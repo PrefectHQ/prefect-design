5ffd5c417a43e3d3449885481e3405f7e63a23c0
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-30 13:55:49 -0400

    Update PCard.vue

--- src/components/Card/PCard.vue
+++ src/components/Card/PCard.vue
@@ -1,28 +1,36 @@
 <template>
-  <div
+  <CardBase
     :class="props.class"
-    class="p-card
-    "
+    :variant="props.variant"
   >
-    <slot />
-  </div>
+    <CardHeader v-if="$slots.header || $slots.title || $slots.description">
+      <CardTitle v-if="$slots.title">
+        <slot name="title" />
+      </CardTitle>
+      <CardDescription v-if="$slots.description">
+        <slot name="description" />
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <slot />
+    </CardContent>
+    <CardFooter v-if="$slots.footer">
+      <slot name="footer" />
+    </CardFooter>
+  </CardBase>
 </template>
 
 <script setup lang="ts">
-  import type { HTMLAttributes } from 'vue'
+  import { type HTMLAttributes } from 'vue'
+  import CardBase, { type CardProps } from '@/components/Card/PCardBase.vue'
+  import CardContent from '@/components/Card/PCardContent.vue'
+  import CardDescription from '@/components/Card/PCardDescription.vue'
+  import CardFooter from '@/components/Card/PCardFooter.vue'
+  import CardHeader from '@/components/Card/PCardHeader.vue'
+  import CardTitle from '@/components/Card/PCardTitle.vue'
 
   const props = defineProps<{
     class?: HTMLAttributes['class'],
+    variant?: CardProps['variant'],
   }>()
 </script>
-
-<style scoped>
-.p-card {
-  @apply
-    rounded-lg
-    border
-    bg-card
-    text-card-foreground
-    shadow-sm;
-}
-</style>
