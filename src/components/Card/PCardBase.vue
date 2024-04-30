17d68a34d0bbf81d62b3f50632ea5806ee023ee6
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-30 13:55:45 -0400

    Create PCardBase.vue

---
+++ src/components/Card/PCardBase.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="p-card"
+    :class="normalizeClass([props.class, card({ variant })])"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { cva, type VariantProps } from 'class-variance-authority'
+  import { normalizeClass, type HTMLAttributes } from 'vue'
+
+  const card = cva(
+    'p-card', {
+      variants: {
+        variant: {
+          ghost: 'p-card-ghost',
+        },
+      },
+      defaultVariants: {
+        variant: null,
+      },
+    },
+  )
+
+  export type CardProps = VariantProps<typeof card>
+
+  const props = defineProps<{
+    class?: HTMLAttributes['class'],
+    variant?: CardProps['variant'],
+  }>()
+</script>
+
+
+<style scoped>
+.p-card {
+  @apply
+    rounded-lg
+    border
+    bg-card
+    text-card-foreground
+    shadow-sm;
+}
+
+.p-card-ghost {
+  @apply
+    bg-transparent
+    border-transparent
+    shadow-none;
+}
+</style>
