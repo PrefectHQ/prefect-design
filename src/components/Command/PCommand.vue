5f4732eff74d21003fa64897c42ff612a2dce652
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-05-02 21:04:40 -0400

    rm unused imports

--- src/components/Command/PCommand.vue
+++ src/components/Command/PCommand.vue
@@ -12,12 +12,8 @@
   import { useForwardPropsEmits, type ComboboxRootEmits, type ComboboxRootProps, ComboboxItem } from 'radix-vue'
   import PCommandBase from '@/components/Command/PCommandBase.vue'
   import PCommandEmpty from '@/components/Command/PCommandEmpty.vue'
-  import PCommandGroup from '@/components/Command/PCommandGroup.vue'
   import PCommandInput from '@/components/Command/PCommandInput.vue'
-  import PCommandItem from '@/components/Command/PCommandItem.vue'
   import PCommandList from '@/components/Command/PCommandList.vue'
-  import PCommandSeparator from '@/components/Command/PCommandSeparator.vue'
-  import PCommandShortcut from '@/components/Command/PCommandShortcut.vue'
 
   // eslint-disable-next-line vue/no-unused-properties
   const props = withDefaults(defineProps<ComboboxRootProps & { placeholder?: string, items?: Record<string, (typeof ComboboxItem)[]> }>(), {
