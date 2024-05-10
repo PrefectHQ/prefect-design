6a46d09a5ad096783148d22cd75f30ea51dd61cf
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-05-10 12:02:48 -0500

    Create PTabsList.vue

---
+++ src/components/Tabs/PTabsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <TabsList
+    v-bind="props"
+    class="p-tabs-list"
+  >
+    <slot />
+  </TabsList>
+</template>
+
+<script setup lang="ts">
+  import { TabsList, type TabsListProps } from 'radix-vue'
+
+  const props = defineProps<TabsListProps>()
+</script>
+
+<style>
+  .p-tabs-list {
+    @apply
+    inline-flex
+    h-10
+    items-center
+    justify-center
+    rounded-md
+    bg-muted
+    p-1
+    text-muted-foreground
+  }
+</style>
